522b2cc013637c775e3348fce4a91cf482bf01b4
Peter Emad <peteremad95@gmail.com>
2023-02-14 11:25:31 +0200

    add the user location address city to the navbar  according to the designs

--- resources/js/Layouts/Partials/Navbar.vue
+++ resources/js/Layouts/Partials/Navbar.vue
@@ -10,6 +10,7 @@ import NotificationComponent from '@/Components/NotificationComponent.vue';
 import {
     BellIcon,
     Bars3Icon,
+    MapPinIcon,
 } from '@heroicons/vue/24/outline';
 
 const showingNavigationDropdown = ref(false);
@@ -22,7 +23,7 @@ const logout = () => {
 <template>
     <nav class="bg-transparent">
         <!-- Primary Navigation Menu -->
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="flex justify-between h-16">
                 <div class="flex">
                     <!-- Navigation Links -->
@@ -37,16 +38,21 @@ const logout = () => {
                     </div>
                 </div>
 
-                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <div class="hidden sm:flex sm:gap-x-3 sm:items-center sm:ml-6">
+                    <!-- user city  -->
+                    <div class="flex items-center gap-1 ">
+                        <MapPinIcon class="w-4 h-4 text-primary" />
+                        <span class="text-white w-max">current user city</span>
+                    </div>
                     <!-- Settings Dropdown -->
-                    <div class="ml-3 relative">
+                    <div class="relative ">
                         <Dropdown align="right" width="48">
                             <template #trigger>
                                 <button
-                                        class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-10 w-10 rounded-full object-cover"
-                                         :src="$page.props.user.avatar_url ? $page.props.user.avatar_url : 'https://ui-avatars.com/api/?name=' + $page.props.user.first_name + ' ' + $page.props.user.last_name + '&color=094609FF&background=E2E2E2'"
-                                         :alt="$page.props.user.name">
+                                    class="flex text-sm transition border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300">
+                                    <img class="object-cover w-10 h-10 rounded-full"
+                                        :src="$page.props.user.avatar_url ? $page.props.user.avatar_url : 'https://ui-avatars.com/api/?name=' + $page.props.user.first_name + ' ' + $page.props.user.last_name + '&color=094609FF&background=E2E2E2'"
+                                        :alt="$page.props.user.name">
                                 </button>
                             </template>
 
@@ -64,7 +70,7 @@ const logout = () => {
                                     Identity Verification
                                 </DropdownLink>
 
-                                <div class="border-t border-gray-100"/>
+                                <div class="border-t border-gray-100" />
 
                                 <!-- Authentication -->
                                 <form @submit.prevent="logout">
@@ -77,11 +83,11 @@ const logout = () => {
                     </div>
 
                     <!-- Notifications Dropdown -->
-                    <div class="ml-3 relative">
+                    <div class="relative ">
                         <Dropdown width="96">
                             <template #trigger>
                                 <button>
-                                    <BellIcon class="h-6 w-6 text-white"></BellIcon>
+                                    <BellIcon class="w-6 h-6 text-white"></BellIcon>
                                 </button>
                             </template>
 
@@ -94,34 +100,35 @@ const logout = () => {
                                 <div v-if="$page.props.notifications.length">
                                     <ul role="list" class="divide-y divide-gray-200">
                                         <template v-for="notification in $page.props.notifications">
-                                            <NotificationComponent :notification="notification"/>
+                                            <NotificationComponent :notification="notification" />
                                         </template>
                                     </ul>
                                 </div>
 
                                 <div v-else>
-                                    <div class="block px-4 py-2 text-xs text-gray-500 text-center">
+                                    <div class="block px-4 py-2 text-xs text-center text-gray-500">
                                         You don't have any new notifications.
                                     </div>
                                 </div>
 
-                                <div class="block px-4 py-2 text-xs text-sky-500 text-center">
-                                    <Link :href="route('notification.index')"
-                                          class="text-sky-500 hover:text-sky-700">View all
+                                <div class="block px-4 py-2 text-xs text-center text-sky-500">
+                                    <Link :href="route('notification.index')" class="text-sky-500 hover:text-sky-700">
+                                    View all
                                     </Link>
                                 </div>
                             </template>
                         </Dropdown>
                     </div>
+
                 </div>
 
                 <!-- Hamburger -->
-                <div class="-mr-2 flex items-center sm:hidden space-x-2">
-                    <div class="ml-3 relative">
+                <div class="flex items-center -mr-2 space-x-2 sm:hidden">
+                    <div class="relative ml-3">
                         <Dropdown width="96">
                             <template #trigger>
                                 <button>
-                                    <BellIcon class="h-6 w-6 text-white"></BellIcon>
+                                    <BellIcon class="w-6 h-6 text-white"></BellIcon>
                                 </button>
                             </template>
 
@@ -134,45 +141,42 @@ const logout = () => {
                                 <div v-if="$page.props.notifications.length">
                                     <ul role="list" class="divide-y divide-gray-200">
                                         <template v-for="notification in $page.props.notifications">
-                                            <NotificationComponent :notification="notification"/>
+                                            <NotificationComponent :notification="notification" />
                                         </template>
                                     </ul>
                                 </div>
 
                                 <div v-else>
-                                    <div class="block px-4 py-2 text-xs text-gray-500 text-center">
+                                    <div class="block px-4 py-2 text-xs text-center text-gray-500">
                                         You don't have any new notifications.
                                     </div>
                                 </div>
 
-                                <div class="block px-4 py-2 text-xs text-sky-500 text-center">
-                                    <Link :href="route('notification.index')"
-                                          class="text-sky-500 hover:text-sky-700">View all
+                                <div class="block px-4 py-2 text-xs text-center text-sky-500">
+                                    <Link :href="route('notification.index')" class="text-sky-500 hover:text-sky-700">
+                                    View all
                                     </Link>
                                 </div>
                             </template>
                         </Dropdown>
                     </div>
                     <button
-                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition"
-                        @click="showingNavigationDropdown = ! showingNavigationDropdown">
-                        <Bars3Icon class="h-6 w-6"/>
+                        class="inline-flex items-center justify-center p-2 text-gray-400 transition rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
+                        @click="showingNavigationDropdown = !showingNavigationDropdown">
+                        <Bars3Icon class="w-6 h-6" />
                     </button>
                 </div>
             </div>
         </div>
 
         <!-- Responsive Navigation Menu -->
-        <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}"
-             class="sm:hidden">
+        <div :class="{ 'block': showingNavigationDropdown, 'hidden': !showingNavigationDropdown }" class="sm:hidden">
             <div class="pt-2 pb-3 space-y-1">
-                <ResponsiveNavLink :href="route('home')"
-                                   :active="route().current('home')">
+                <ResponsiveNavLink :href="route('home')" :active="route().current('home')">
                     Home
                 </ResponsiveNavLink>
 
-                <ResponsiveNavLink :href="route('invitation.index')"
-                                   :active="route().current('invitation.index')">
+                <ResponsiveNavLink :href="route('invitation.index')" :active="route().current('invitation.index')">
                     Invitations
                 </ResponsiveNavLink>
             </div>
@@ -180,33 +184,32 @@ const logout = () => {
             <!-- Responsive Settings Options -->
             <div class="pt-4 pb-1 border-t border-gray-200">
                 <div class="flex items-center px-4">
-                    <div v-if="$page.props.jetstream.managesProfilePhotos" class="shrink-0 mr-3">
-                        <img class="h-10 w-10 rounded-full object-cover"
-                             :src="'/' + $page.props.user.avatar" :alt="$page.props.user.name">
+                    <div v-if="$page.props.jetstream.managesProfilePhotos" class="mr-3 shrink-0">
+                        <img class="object-cover w-10 h-10 rounded-full" :src="'/' + $page.props.user.avatar"
+                            :alt="$page.props.user.name">
                     </div>
 
                     <div>
-                        <div class="font-medium text-base text-gray-800">
+                        <div class="text-base font-medium text-gray-800">
                             {{ $page.props.user.username }}
                         </div>
-                        <div class="font-medium text-sm text-gray-500">
+                        <div class="text-sm font-medium text-gray-500">
                             {{ $page.props.user.email }}
                         </div>
                     </div>
                 </div>
 
                 <div class="mt-3 space-y-1">
-                    <ResponsiveNavLink :href="route('profile.show')"
-                                       :active="route().current('profile.show')">
+                    <ResponsiveNavLink :href="route('profile.show')" :active="route().current('profile.show')">
                         Profile
                     </ResponsiveNavLink>
 
                     <ResponsiveNavLink :href="route('identity.verification.create')"
-                                       :active="route().current('identity.verification.create')">
+                        :active="route().current('identity.verification.create')">
                         Identity Verification
                     </ResponsiveNavLink>
 
-                    <div class="border-t border-gray-100"/>
+                    <div class="border-t border-gray-100" />
 
                     <form method="POST" @submit.prevent="logout">
                         <ResponsiveNavLink as="button">
