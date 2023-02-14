484689a070038525c370aa55b05273321b64dca0
Peter Emad <peteremad95@gmail.com>
2023-02-14 13:10:46 +0200

    create the chat index vue page

---
+++ resources/js/Pages/Chat/Index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import AppLayout from '../../Layouts/AppLayout.vue';
+
+
+</script>
+<template>
+    <AppLayout :title="chat">
+        <template #header>
+            <p>chat</p>
+        </template>
+
+
+    </AppLayout>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
