4faff4b8b2f4dc59844ee417f189c07c9c142688
Osama Bassam <osama.m.bassam@gmail.com>
2023-02-19 12:15:55 +0200

    wip

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -53,6 +53,15 @@ class ChatController extends Controller
     {
         $query = $conversation->messages()->orderBy('created_at', 'DESC');
 
+        $conversations = Conversation::query()->whereHas('users', function ($query) use ($conversation) {
+            $query->where('conversation_user.conversation_id', $conversation->id)
+                ->whereNot('conversation_user.user_id', request()->user()->id);
+        })
+            ->with('messages')
+            ->with('users', function ($query) {
+                $query->whereNot('conversation_user.user_id', request()->user()->id);
+            })->get();
+
         if (request()->has('search')) {
             $query->where('body', '%LIKE%', request()->input('search'));
         }
@@ -60,13 +69,7 @@ class ChatController extends Controller
         return Inertia::render('Chat/Show', [
             'messages' => $query->paginate(12),
             'conversation' => $conversation,
-            'conversations' => Conversation::query()->whereHas('users', function ($query) use ($conversation) {
-                $query->where('conversation_user.conversation_id', $conversation->id)
-                    ->whereNot('conversation_user.user_id', request()->user()->id)->with('messages')
-                    ->with('users', function ($query) {
-                        $query->whereNot('conversation_user.user_id', request()->user()->id);
-                    })->get();
-            })
+            'conversations' => $conversations
         ]);
     }
 }
