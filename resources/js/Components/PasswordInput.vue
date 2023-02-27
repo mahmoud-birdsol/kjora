4c90996f8a6422b381733764c6fb8d233e741a9f
Peter Emad <peteremad95@gmail.com>
2023-02-27 10:39:22 +0200

    hide the password input eye icon fix the nav photo

--- resources/js/Components/PasswordInput.vue
+++ resources/js/Components/PasswordInput.vue
@@ -24,19 +24,14 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <div class="relative mt-1 rounded-md">
-        <input
-            ref="input"
-            :type="type"
-            :value="modelValue"
-            :placeholder="placeholder"
-            class="block w-full rounded-full border-gray-300 px-4 shadow-sm focus:border-primary focus:ring-primary sm:text-sm disabled:bg-gray-100"
-            @input="$emit('update:modelValue', $event.target.value)"
-            auto-complete="new-password"
-            aria-required="true"
-        >
+        <input ref="input" :type="type" :value="modelValue" :placeholder="placeholder"
+            class="removeEyeIcon block w-full rounded-full border-gray-300 px-4 shadow-sm focus:border-primary focus:ring-primary sm:text-sm disabled:bg-gray-100"
+            @input="$emit('update:modelValue', $event.target.value)" auto-complete="new-password" aria-required="true">
         <div class="cursor-pointer absolute inset-y-0 right-0 flex items-center pr-3">
-            <EyeSlashIcon v-if="type == 'text'" @click="type = 'password'" class="h-5 w-5 text-gray-400"/>
-            <EyeIcon v-if="type == 'password'" @click="type = 'text'" class="h-5 w-5 text-gray-400"/>
+            <EyeSlashIcon v-if="type == 'text'" @click="type = 'password'" class="h-5 w-5 text-gray-400" />
+            <EyeIcon v-if="type == 'password'" @click="type = 'text'" class="h-5 w-5 text-gray-400" />
         </div>
     </div>
 </template>
+
+<style></style>
