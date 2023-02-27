48730fcc5ffe013c7d5904f27a83b73d514899b5
Peter Emad <peteremad95@gmail.com>
2023-02-27 17:56:41 +0200

    review page ui

--- resources/js/Components/PerformanceTab.vue
+++ resources/js/Components/PerformanceTab.vue
@@ -1,37 +1,12 @@
 <template>
     <div class="text-xl text-center text-white capitalize">
-        <Radar :data="[28, 48, 40, 19, 96, 27]"/>
+        <RatingChart :data="[1, 2, 3, 4, 5, 1.5]" />
     </div>
 </template>
 
 <script setup>
-import Radar from './Radar.vue';
-    // scales: {
-    //     r: {
-    //         max: 5,
-    //         min: 1,
-    //         ticks: {
-    //             count:9,
-    //             stepSize: 0.5,
-    //             color: '#000',
-    //             backdropColor: 'transparent',
-    //             z: 100,
-    //         },
-    //         grid: {
-    //             color: '#DBDBDB',
-    //             lineWidth: 8,
-    //             circular: true,
-    //         },
-    //         angleLines: {
-    //             display: false
-    //         },
-    //         pointLabels: {
-    //             color: '#000',
-    //         },
-    //         backgroundColor: 'rgb(9, 70, 9)',
-    //         beginAtZero: true,
-    //     },
-    // },
+import RatingChart from './RatingChart.vue';
+
 
 </script>
 
