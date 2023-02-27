531d1a62494f428a714018656076093b0654a829
aml walaed <amlwalaed@gmail.com>
2023-02-27 15:16:19 +0200

    make compoent for radar

--- resources/js/Components/PerformanceTab.vue
+++ resources/js/Components/PerformanceTab.vue
@@ -1,75 +1,11 @@
 <template>
     <div class="text-xl text-center text-white capitalize">
-        <Radar :data="data" :options="options" />
+        <Radar :data="[28, 48, 40, 19, 96, 27]"/>
     </div>
 </template>
 
 <script setup>
-import {
-    Chart as ChartJS,
-    RadialLinearScale,
-    PointElement,
-    LineElement,
-    Filler,
-    Tooltip,
-} from 'chart.js'
-import { Radar } from 'vue-chartjs'
-
-ChartJS.register(
-    RadialLinearScale,
-    PointElement,
-    LineElement,
-    Filler,
-    Tooltip,
-)
-const data = {
-    labels: [
-        'Agility',
-        'Stamina',
-        'Sttrngth',
-        'Passing',
-        'Shooting',
-        'pace',
-    ],
-    datasets: [
-  {
-    label: 'My Second Dataset',
-    data: [28, 48, 40, 19, 96, 27],
-    fill: true,
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
-    borderColor: 'rgb(255, 255, 255)',
-    pointBackgroundColor: 'rgb(255, 255, 255)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgb(54, 162, 235)'
-  }
-]
-}
-const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    elements: {
-      line: {
-        borderWidth: 3
-      }
-    },
-    scales:{
-        r:{
-            grid: {
-                color: '#DBDBDB',
-                lineWidth: 1,
-                // circular: true,
-            },
-            ticks: {
-                color: '#FFF',
-                backdropColor: 'transparent',
-                z: 100,
-            },
-            pointLabels: {
-                color: '#FFF',
-            },
-        }
-    }
+import Radar from './Radar.vue';
     // scales: {
     //     r: {
     //         max: 5,
@@ -96,7 +32,7 @@ const options = {
     //         beginAtZero: true,
     //     },
     // },
-}
+
 </script>
 
 <style lang="scss" scoped></style>
---
+++ resources/js/Components/Radar.vue
@@ -0,0 +1,78 @@
+<script setup>
+import {
+    Chart as ChartJS,
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+} from 'chart.js'
+import { Radar } from 'vue-chartjs'
+
+ChartJS.register(
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+)
+let props = defineProps({
+    labels:{
+        type:Array,
+        default: [
+        'Agility',
+        'Stamina',
+        'Sttrngth',
+        'Passing',
+        'Shooting',
+        'pace',
+    ]
+    },
+    data: Array
+})
+const data = {
+    labels: props.labels,
+    datasets: [
+  {
+    label: 'My Second Dataset',
+    data: props.data,
+    fill: true,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    borderColor: 'rgb(255, 255, 255)',
+    pointBackgroundColor: 'rgb(255, 255, 255)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(54, 162, 235)'
+  }
+]
+}
+const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    elements: {
+      line: {
+        borderWidth: 3
+      }
+    },
+    scales:{
+        r:{
+            grid: {
+                color: '#DBDBDB',
+                lineWidth: 1,
+                // circular: true,
+            },
+            ticks: {
+                color: '#FFF',
+                backdropColor: 'transparent',
+                z: 100,
+            },
+            pointLabels: {
+                color: '#FFF',
+            },
+        }
+    }
+}
+</script>
+<template>
+    <Radar :data="data" :options="options" />
+</template>
