86f0e9ebc64e11cceacf7655b5fcd3f12e5fa1c5
Peter Emad <peteremad95@gmail.com>
2023-03-02 12:56:33 +0200

    fix chat bugs

--- resources/js/Components/Chat/ChatMessages.vue
+++ resources/js/Components/Chat/ChatMessages.vue
@@ -1,8 +1,9 @@
 <script setup>
-import {onMounted, onUnmounted, ref} from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 import ChatMessage from "@/Components/Chat/ChatMessage.vue";
 import ChatNotice from "@/Components/Chat/ChatNotice.vue";
-import {useChat} from "@/stores/chat";
+import { useChat } from "@/stores/chat";
+import { usePage } from "@inertiajs/inertia-vue3";
 
 const props = defineProps({
     conversation: {
@@ -17,11 +18,13 @@ const props = defineProps({
 
 const chat = useChat();
 const messagesContainer = ref(null);
-
 onMounted(() => {
+    const currentUser = usePage().props.value.auth.user
+    console.log(currentUser.id)
     chat.initialize({
         conversation: props.conversation,
         container: messagesContainer.value,
+        currentUserId: currentUser.id
     });
 });
 
@@ -31,15 +34,14 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div ref="messagesContainer"
-         v-loading="chat.isLoading"
-         class="relative flex flex-col gap-y-4 overflow-auto p-2 min-h-[300px] max-h-[350px] md:min-h-[400px] md:max-h-[450px]">
+    <div ref="messagesContainer" v-loading="chat.isLoading"
+        class="relative flex flex-col gap-y-4 overflow-auto p-2 min-h-[300px] max-h-[350px] md:min-h-[400px] md:max-h-[450px]">
 
         <ChatNotice v-if="chat.lastPageReached">No more messages to load.</ChatNotice>
         <ChatNotice v-if="chat.isLoading">Loading.</ChatNotice>
 
         <template v-for="message in [...chat.messageList].reverse()" :key="message.id">
-            <ChatMessage :message="message" :player="player"/>
+            <ChatMessage :message="message" :player="player" />
         </template>
     </div>
 </template>
