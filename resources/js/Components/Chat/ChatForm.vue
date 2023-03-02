9ec8a9dde3c19d19493ca31533b2474544f1c973
Osama Bassam <osama.m.bassam@gmail.com>
2023-03-02 12:10:48 +0200

    fixed media

--- resources/js/Components/Chat/ChatForm.vue
+++ resources/js/Components/Chat/ChatForm.vue
@@ -145,10 +145,10 @@ const removePhoto = (i) => {
                 </div>
                 <div>
                     <div class="text-black max-w-[15rem]">
-                        <div v-if="chat.repliedMessage.media.length">
-                            <MediaPreview :fileType="chat.repliedMessage.media[0].mime_type"
-                                :filePreview="chat.repliedMessage.media[0].original_url"
-                                :fileName="chat.repliedMessage.media[0].name" />
+                        <div v-if="chat.repliedMessage.attachments.length">
+                            <MediaPreview :fileType="chat.repliedMessage.attachments[0].mime_type"
+                                :filePreview="chat.repliedMessage.attachments[0].original_url"
+                                :fileName="chat.repliedMessage.attachments[0].name" />
                         </div>
 
                     </div>
