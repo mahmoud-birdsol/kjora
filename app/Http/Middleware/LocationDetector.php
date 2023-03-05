da29085e5a212770a00ac9b36fecf82c1055d0c8
Peter Emad <peteremad95@gmail.com>
2023-03-05 15:17:03 +0200

    CU-8669nuxp3 - As a user, when I click on any user username it should redirect me to their profile

--- app/Http/Middleware/LocationDetector.php
+++ app/Http/Middleware/LocationDetector.php
@@ -19,15 +19,16 @@ class LocationDetector
     {
         $user = $request->user();
         $userClintIp = \Request::ip();
-        $userClintIp = '24.48.0.2';
-        if($user->last_known_ip != $userClintIp || $user->last_known_ip == null ){
-            $userGeolocationData = \App\Services\Geolocator\IpInfo::locate( $userClintIp);
-            $user->setAttribute( 'last_known_ip' , $userGeolocationData->ip() );
-            $user->setAttribute( 'current_country' , $userGeolocationData->country());
-            $user->setAttribute( 'current_region' , $userGeolocationData->region() );
-            $user->setAttribute( 'current_city' , $userGeolocationData->city() );
-            $user->setAttribute( 'current_latitude' , $userGeolocationData->latitude());
-            $user->setAttribute( 'current_longitude' , $userGeolocationData->longitude());
+        if ($userClintIp == '127.0.0.1')
+            $userClintIp = '24.48.0.2';
+        if ($user->last_known_ip != $userClintIp || $user->last_known_ip == null) {
+            $userGeolocationData = \App\Services\Geolocator\IpInfo::locate($userClintIp);
+            $user->setAttribute('last_known_ip', $userGeolocationData->ip());
+            $user->setAttribute('current_country', $userGeolocationData->country());
+            $user->setAttribute('current_region', $userGeolocationData->region());
+            $user->setAttribute('current_city', $userGeolocationData->city());
+            $user->setAttribute('current_latitude', $userGeolocationData->latitude());
+            $user->setAttribute('current_longitude', $userGeolocationData->longitude());
             $user->save();
         }
         return $next($request);
