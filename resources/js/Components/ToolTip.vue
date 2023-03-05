6a6c820e299eab8b951665d87ff2f1c2b4261669
aml walaed <amlwalaed@gmail.com>
2023-03-05 17:40:49 +0200

    commit

---
+++ resources/js/Components/ToolTip.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+    value:String
+})
+</script>
+<template>
+
+        <div class="absolute top-full left-1/2 -translate-x-1/2 bg-black bg-opacity-50 text-white p-2 rounded-lg text-xs z-10 block whitespace-nowrap">
+            {{ value }}
+        </div>
+</template>
