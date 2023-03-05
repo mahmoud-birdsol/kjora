dceddc11c37f31b630be8a4271402db04afab04e
aml walaed <amlwalaed@gmail.com>
2023-03-05 16:18:25 +0200

    CU-8669nuwfr - As a user, I want to filter my invitations, or hires by date and time, from-to.

--- resources/js/Pages/Invitation/Index.vue
+++ resources/js/Pages/Invitation/Index.vue
@@ -3,10 +3,36 @@ import { Head, Link } from '@inertiajs/inertia-vue3';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 import InvitationCard from "./Partials/InvitationCard.vue";
-
+import {
+    XMarkIcon,
+    AdjustmentsHorizontalIcon,
+} from '@heroicons/vue/24/outline';
+import Modal from '@/Components/Modal.vue';
+import { ref } from 'vue'
+import { ElDatePicker, ElTimePicker } from 'element-plus';
+import InputLabel from '@/Components/InputLabel.vue';
+import { useForm, usePage } from '@inertiajs/inertia-vue3';
 const props = defineProps({
     invitations: Array,
 });
+let showFiltersModal = ref(false)
+const form = useForm({
+    dateFrom: null,
+    dateTo: null,
+});
+
+const filter = () => {
+    loading.value = true;
+
+    form.get(route('invitations'), {
+        preserveState: true,
+        preserveScroll: true,
+        onSuccess: () => {
+            loading.value = false;
+            showFiltersModal.value = false;
+        }
+    });
+};
 </script>
 
 <template>
@@ -47,5 +73,55 @@ const props = defineProps({
                 </div>
             </div>
         </div>
+        <div class="fixed bottom-0 right-0 p-10 sm:px-20 lg:px-40">
+            <button class="flex items-center justify-center w-16 h-16 text-center bg-black rounded-full shadow-xl"
+                @click="showFiltersModal = !showFiltersModal">
+                <AdjustmentsHorizontalIcon class="w-10 h-10 text-white" />
+            </button>
+            <Modal :show="showFiltersModal" max-width="sm" @close="showFiltersModal = false" :closeable="false">
+                <div class="p-6 bg-black">
+                    <div class="flex items-center justify-between">
+                        <p class="text-sm text-white">Filter </p>
+
+                        <button @click="showFiltersModal = false">
+                            <XMarkIcon class="w-4 h-4 text-white" />
+                        </button>
+                    </div>
+
+                    <form @submit.prevent="filter">
+                        <div class="my-6">
+                            <InputLabel>Date From : </InputLabel>
+                            <ElDatePicker v-model="form.dateFrom" class="w-full" type="datetime" placeholder="Pick a Date"
+                                format="YYYY/MM/DD hh:mm:ss" value-format="YYYY-MM-DD h:m:s a" />
+                        </div>
+                        <div class="my-6">
+                            <InputLabel>Date To :</InputLabel>
+                            <ElDatePicker v-model="form.dateFrom" class="w-full" type="datetime" placeholder="Pick a Date"
+                                format="YYYY/MM/DD hh:mm:ss" value-format="YYYY-MM-DD h:m:s a" />
+                        </div>
+
+                        <div class="my-6 mt-4">
+                            <SecondaryButton @click="filter">Apply</SecondaryButton>
+                        </div>
+                        <div class="flex items-center justify-center mt-4">
+                            <button class="text-primary" @click="reset">
+                                <!-- <XMarkIcon class="inline w-4 h-4 mr-4" /> -->
+                                Reset
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </Modal>
+        </div>
     </AppLayout>
 </template>
+<style>
+.el-input__wrapper  {
+    background-color: black;
+    color: white;
+}
+.el-input__inner{
+    color: white;
+    
+}
+</style>
