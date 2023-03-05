894382ac7088a59846cb407d161537eaf185ddaa
Peter Emad <peteremad95@gmail.com>
2023-03-05 22:29:21 +0200

    redirect to profile page after deleting media

--- resources/js/Pages/Gallery/Show.vue
+++ resources/js/Pages/Gallery/Show.vue
@@ -4,17 +4,17 @@
             <p v-if="media.mime_type.startsWith('image') || media.mime_type.startsWith('webp')">Photo</p>
             <p v-if="media.mime_type.startsWith('video')">video</p>
         </template>
-        <div class="bg-white rounded-3xl px-2 xs:px-8 py-6">
-            <div class="w-full grid lg:grid-cols-2 gap-3  border rounded-2xl border-stone-400   ">
+        <div class="px-2 py-6 bg-white rounded-3xl xs:px-8">
+            <div class="grid w-full gap-3 border lg:grid-cols-2 rounded-2xl border-stone-400 ">
 
                 <!-- image and caption left col -->
-                <div class=" flex flex-col gap-6 max-w-full  p-3 ">
+                <div class="flex flex-col max-w-full gap-6 p-3 ">
                     <!-- image -->
-                    <div class="rounded-3xl  sm:aspect-video overflow-hidden flex justify-center">
+                    <div class="flex justify-center overflow-hidden rounded-3xl sm:aspect-video">
                         <img v-if="media.mime_type.startsWith('image') || media.mime_type.startsWith('webp')"
-                            :src="media.original_url" alt="" class="h-full  object-contain  ">
+                            :src="media.original_url" alt="" class="object-contain h-full ">
                         <video v-if="media.mime_type.startsWith('video')" controls :src="media.original_url" alt=""
-                            class="object-cover   " />
+                            class="object-cover " />
                     </div>
                     <!-- information -->
                     <div class="grid xs:grid-cols-[min-content_1fr] gap-4">
@@ -26,10 +26,10 @@
                         <!-- col 2 -->
                         <div class="flex flex-col gap-3">
                             <!-- user information row 1-->
-                            <div class="flex w-full justify-between">
+                            <div class="flex justify-between w-full">
                                 <div class="flex flex-col gap-1 ">
                                     <div class="flex flex-row gap-2">
-                                        <h3 class="font-bold m-0 text-lg leading-none  capitalize ">{{ user.name }} </h3>
+                                        <h3 class="m-0 text-lg font-bold leading-none capitalize ">{{ user.name }} </h3>
                                         <span v-if="false">star icon</span>
                                     </div>
                                     <Link :href="route('player.profile', user.id)" class="text-xs text-stone-400 ">@{{
@@ -42,11 +42,11 @@
                                     <FadeInTransition>
                                         <!-- media option menu -->
                                         <div v-show="showOptions"
-                                            class="absolute z-20 px-6 py-2 text-xs text-white top-0 right-8 bg-black border rounded-xl border-neutral-500 pie-10 z-2 ">
+                                            class="absolute top-0 z-20 px-6 py-2 text-xs text-white bg-black border right-8 rounded-xl border-neutral-500 pie-10 z-2 ">
                                             <ul class="flex flex-col justify-center gap-y-2">
                                                 <button class="hover:text-gray-400 group" @click="editCaption">
-                                                    <li class="flex items-center  gap-x-2">
-                                                        <PencilIcon class=" w-4" />
+                                                    <li class="flex items-center gap-x-2">
+                                                        <PencilIcon class="w-4 " />
                                                         <span>Edit</span>
                                                     </li>
                                                 </button>
@@ -59,15 +59,15 @@
                                                     <Modal :show="showDeleteMediaModal"
                                                         @close="showDeleteMediaModal = false" :closeable="true"
                                                         :show-close-icon="false" :max-width="'sm'">
-                                                        <div class="text-stone-800 p-6 flex flex-col justify-center ">
+                                                        <div class="flex flex-col justify-center p-6 text-stone-800 ">
                                                             <p class="mb-3 text-lg">Are you sure you want delete this
                                                                 media?</p>
-                                                            <div class="flex gap-4 w-full justify-center">
+                                                            <div class="flex justify-center w-full gap-4">
                                                                 <button
-                                                                    class="p-2 px-8 border-primary border-2 hover:bg-primary text-primary hover:text-white active:scale-95 rounded-full "
+                                                                    class="p-2 px-8 border-2 rounded-full border-primary hover:bg-primary text-primary hover:text-white active:scale-95 "
                                                                     @click="showDeleteMediaModal = false">Cancel</button>
                                                                 <button
-                                                                    class="p-2 px-8 border-red-800 border-2 bg-red-800 hover:bg-transparent hover:text-red-800 text-white active:scale-95  rounded-full "
+                                                                    class="p-2 px-8 text-white bg-red-800 border-2 border-red-800 rounded-full hover:bg-transparent hover:text-red-800 active:scale-95 "
                                                                     @click="removeMedia">Delete</button>
 
                                                             </div>
@@ -82,7 +82,7 @@
                                 </div>
                             </div>
                             <!-- date and time and likes row 2-->
-                            <div class="flex w-full gap-2 text-sm justify-between text-stone-700">
+                            <div class="flex justify-between w-full gap-2 text-sm text-stone-700">
                                 <div class="flex flex-row gap-2">
                                     <span>{{ dayjs(media.created_at).fromNow(true) }}</span>
                                     <span>|</span>
@@ -92,7 +92,7 @@
                                 <div class="flex items-center gap-1"><span class="text-sm">{{ '10' }}</span>
                                     <button class="p-1">
                                         <HeartIcon
-                                            class="w-5 text-primary hover:fill-current fill-transparent stroke-current stroke-2" />
+                                            class="w-5 stroke-current stroke-2 text-primary hover:fill-current fill-transparent" />
                                     </button>
                                 </div>
                             </div>
@@ -103,7 +103,7 @@
                                 </p>
                                 <div v-show="isEditingCaption" class="flex">
                                     <textarea rows="4" type="text" :value="caption" @blur="submit"
-                                        class="text-sm text-stone-500 w-full ring-1 focus:ring-primary focus:shadow-none focus:border-none border-none ring-gray-300 hideScrollBar" />
+                                        class="w-full text-sm border-none text-stone-500 ring-1 focus:ring-primary focus:shadow-none focus:border-none ring-gray-300 hideScrollBar" />
                                 </div>
                             </div>
 
@@ -111,21 +111,21 @@
                     </div>
                 </div>
                 <!-- comment and replies right col -->
-                <div class="flex flex-col gap-2 h-full max-lg:border-t lg:border-l border-stone-300">
+                <div class="flex flex-col h-full gap-2 max-lg:border-t lg:border-l border-stone-300">
                     <!-- header -->
-                    <div class=" pt-5 p-3 text-sm border-b border-stone-300">comments {{ comments && comments.filter(c =>
+                    <div class="p-3 pt-5 text-sm border-b border-stone-300">comments {{ comments && comments.filter(c =>
                         !c.parent_id)?.length }}
                     </div>
                     <!-- comments -->
-                    <div class="self-stretch p-3 px-6 h-full ">
-                        <div class="flex flex-col gap-4 w-full" v-if="comments">
+                    <div class="self-stretch h-full p-3 px-6 ">
+                        <div class="flex flex-col w-full gap-4" v-if="comments">
                             <template v-for="comment in comments.filter(c => !c.parent_id)" :key="comment.id">
                                 <Comment @addedReply="handleAddedReply" :comment="comment"></Comment>
                             </template>
                         </div>
                     </div>
                     <!-- new comment form -->
-                    <div class="flex flex-row p-3 items-center self-end w-full gap-x-3 border-t border-stone-300">
+                    <div class="flex flex-row items-center self-end w-full p-3 border-t gap-x-3 border-stone-300">
                         <button>
                             <FaceSmileIcon class="w-6 text-neutral-400" />
                         </button>
@@ -133,7 +133,7 @@
 
                             <textarea @keypress.enter.exact.prevent="addComment" v-model="newComment" name="newComment"
                                 id="newComment" rows="1" placeholder="Add a comment..."
-                                class="w-full p-2 px-4 border-none rounded-full resize-none hideScrollBar placeholder:text-neutral-400 bg-stone-100 text-stone-700 focus:ring-1 focus:ring-primary  "></textarea>
+                                class="w-full p-2 px-4 border-none rounded-full resize-none hideScrollBar placeholder:text-neutral-400 bg-stone-100 text-stone-700 focus:ring-1 focus:ring-primary "></textarea>
                         </div>
 
                         <button @click="addComment" :disabled="isSending" class="p-1 group ">
@@ -162,6 +162,7 @@ import relativeTime from 'dayjs/plugin/relativeTime.js';
 import { usePage, Link } from '@inertiajs/inertia-vue3';
 import FadeInTransition from '../../Components/FadeInTransition.vue';
 import Modal from '../../Components/Modal.vue';
+import { Inertia } from '@inertiajs/inertia';
 
 
 onBeforeMount(() => {
@@ -227,6 +228,7 @@ function handleAddedReply() {
 // delete media
 function removeMedia() {
     axios.delete(route('api.gallery.destroy', props.media.id)).then((res) => console.log(res))
+    Inertia.get(route('profile.show'))
 }
 
 function openRemoveMediaModal() {
