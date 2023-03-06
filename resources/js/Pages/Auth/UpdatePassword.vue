ae92b7d52f05cc780c54209c8f4fcc74eefc461e
aml walaed <amlwalaed@gmail.com>
2023-03-06 15:23:33 +0200

    add small text

--- resources/js/Pages/Auth/UpdatePassword.vue
+++ resources/js/Pages/Auth/UpdatePassword.vue
@@ -15,14 +15,14 @@ const form = useForm({
     current_password: null
 })
 function submit() {
-    loading.value=true
+    loading.value = true
     form.patch(route('password.update'), {
         preserveState: true,
         preserveScroll: true,
         onSuccess: () => {
-            loading.value = false;
         }
     })
+    loading.value = false;
 }
 </script>
 <template>
@@ -34,22 +34,25 @@ function submit() {
             <Card class="md:w-1/2" v-loading="loading">
                 <CardContent title="update Password">
                     <template #body>
-                            <div class="text-sm text-gray-500 text-center py-10">
-                                please enter your current and new password to update your password
+                        <div class="text-sm text-gray-500 text-center py-10">
+                            please enter your current and new password to update your password
+                        </div>
+                        <div class="flex flex-col gap-4 my-10">
+                            <div>
+                                <InputLabel value="current password" color="primary" />
+                                <PasswordInput v-model="form.current_password" placeholder="enter your current password" />
+                                <InputError class="mt-2" :message="form.errors.current_password" />
                             </div>
-                            <div class="flex flex-col gap-4 my-10">
-                                <div>
-                                    <InputLabel value="current password" color="primary" />
-                                    <PasswordInput  v-model="form.current_password" placeholder="enter your current password"/>
-                                    <InputError class="mt-2" :message="form.errors.current_password" />
-
-                                </div>
-                                <div>
-                                    <InputLabel value="new password" color="primary" />
-                                    <PasswordInput v-model="form.new_password" placeholder="enter your new password"/>
-                                    <InputError class="mt-2" :message="form.errors.new_password" />
+                            <div>
+                                <InputLabel value="new password" color="primary" />
+                                <PasswordInput v-model="form.new_password" placeholder="enter your new password" />
+                                <InputError class="mt-2" :message="form.errors.new_password" />
+                                <div class="text-gray-400 text-xs m-2" v-if="!form.password && !form.errors.password">
+                                    The password must be at least 8 characters and at least one uppercase, one lowercase
+                                    letter , one symbol and one number
                                 </div>
                             </div>
+                        </div>
                     </template>
                     <template #footer>
                         <PrimaryButton @click="submit" class="align-bottom">Upload</PrimaryButton>
