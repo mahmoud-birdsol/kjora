37cf4f453653077009cd8fe71ab0e85e4cf0726d
ahmy1996404 <a.hamodys@yahoo.com>
2023-03-06 11:46:43 +0200

    CU-8669nvah4/As-a-user-I-want-to-update-my-password-in-the-settings-page

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Services\FlashMessage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -36,17 +37,29 @@ class PasswordController extends Controller
         ]);
         #Match The Old Password
         if(!Hash::check($request->current_password, auth()->user()->password)){
-            return back()->with("error", "Old Password Doesn't match!");
+            $request->session()->flash('message', [
+                'type' => 'error',
+                'body' => "Old Password Doesn't match!",
+            ]);
+            return redirect()->back();
         }
 
         if(strcmp($request->get('current_password'), $request->get('new_password')) == 0){
             // Current password and new password same
-            return redirect()->back()->with("error","New Password cannot be same as your current password.");
+            $request->session()->flash('message', [
+                'type' => 'error',
+                'body' => 'New Password cannot be same as your current password.',
+            ]);
+            return redirect()->back();
         }
         $user = Auth::user();
-        $user->password =  Hash::make($request->get('new_password'));
-        $user->save();
-
+        $user->update([
+            'password' => Hash::make($request->get('new_password'))
+        ]);
+        $request->session()->flash('message', [
+            'type' => 'success',
+            'body' => 'Password changed successfully',
+        ]);
         return back();
     }
 }
