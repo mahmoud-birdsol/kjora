6b13107b40647a7ae587d285aeba795e66472a37
Peter Emad <peteremad95@gmail.com>
2023-03-09 15:51:17 +0200

    add the map view on the home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -12,7 +12,7 @@ import Pagination from '@/Components/Pagination.vue';
 import MainPlayerCard from '@/Components/PlayerCards/MainPlayerCard.vue';
 import HelloUserHeader from '@/Components/HelloUserHeader.vue';
 import { Splide, SplideSlide } from "@splidejs/vue-splide";
-
+import PlayersMap from '@/Components/Maps/PlayersMap.vue';
 import {
     XMarkIcon,
     AdjustmentsHorizontalIcon,
@@ -24,6 +24,9 @@ const props = defineProps({
     positions: Array,
     advertisements: Array
 });
+
+
+const currentTabId = ref(1)
 
 const form = useForm({
     position: usePage().props.value.queryParams.position ?? null,
@@ -82,6 +85,10 @@ const options = {
 
     },
 };
+
+
+
+
 </script>
 
 <template>
@@ -93,7 +100,8 @@ const options = {
         </template>
         <template #ads>
 
-            <Splide dir="ltr" class=" h-[4rem] w-[32rem] max-w-full self-end overflow-hidden  rounded-full md:ml-auto" :options="options">
+            <Splide dir="ltr" class=" h-[4rem] w-[32rem] max-w-full self-end overflow-hidden  rounded-full md:ml-auto"
+                :options="options">
                 <template v-for="(advertisement, i) in advertisements" :key="i">
                     <SplideSlide class="h-full">
                         <img class="object-cover h-full " :src="advertisement" alt="">
@@ -105,19 +113,19 @@ const options = {
         </template>
         <div class="">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <!-- Position Filters...
-                                                                                                    =====================================================-->
+                <!-- Position Filters... -->
                 <div class="flex gap-4 mt-4 mb-8 overflow-x-auto hideScrollBar">
-                    <button @click="filterByPosition(null)" class="py-2 px-4  min-w-[215px] w-1/5 font-bold  text-center items-center bg-white border-2 border-gray-300 rounded-full text-xs  text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-primary active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition whitespace-nowrap"
-                    :class="{ 'border-primary': form.position == null, 'border-none': form.position != null }"
-                    >
+                    <button @click="filterByPosition(null)"
+                        class="py-2 px-4  min-w-[215px] w-1/5 font-bold  text-center items-center bg-white border-2 border-gray-300 rounded-full text-xs  text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-primary active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition whitespace-nowrap"
+                        :class="{ 'border-primary': form.position == null, 'border-none': form.position != null }">
                         <span class="w-full text-center"
                             :class="{ 'text-black': form.position == null, 'text-gray-400': form.position != null }">
                             All positions
                         </span>
                     </button>
                     <template v-for="position in positions" :key="position.id">
-                        <button @click="filterByPosition(position.id)" class="py-2 px-4 min-w-[215px] w-1/5 text-center font-bold items-center bg-white border-2 border-gray-300 rounded-full  text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-primary   active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition whitespace-nowrap">
+                        <button @click="filterByPosition(position.id)"
+                            class="py-2 px-4 min-w-[215px] w-1/5 text-center font-bold items-center bg-white border-2 border-gray-300 rounded-full  text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-primary   active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition whitespace-nowrap">
                             <span class="w-full text-center"
                                 :class="{ 'text-black': form.position == position.id, 'text-gray-400': form.position != position.id }">
                                 {{ position.name }}
@@ -125,28 +133,38 @@ const options = {
                         </button>
                     </template>
                 </div>
+                <div class="flex justify-end gap-3 mb-2 w-ful ">
+                    <button @click="currentTabId = 1"
+                        class="p-2 px-4 font-bold uppercase bg-white rounded-lg cursor-pointer hover:bg-stone-100 active:scale-95 "
+                        :class="currentTabId == 1 ? 'bg-stone-300' : ''">grid</button>
+                    <button @click="currentTabId = 2"
+                        class="p-2 px-4 font-bold uppercase bg-white rounded-lg cursor-pointer hover:bg-stone-100 active:scale-95 "
+                        :class="currentTabId == 2 ? 'bg-stone-300' : ''">map</button>
 
-                <!-- Current list...
-                                                                                                    =====================================================-->
-                <div class="bg-white min-h-[500px] overflow-hidden shadow-xl sm:rounded-lg p-6" v-loading="loading">
-
-                    <div class="flex items-start justify-start my-6">
-                        <p class="text-sm font-bold">Total ({{ players.total }})</p>
+                </div>
+                <!-- Current list...-->
+                <div v-show="currentTabId == 1" class="bg-white min-h-[500px] overflow-hidden shadow-xl sm:rounded-lg p-6"
+                    v-loading="loading">
+                    <div>
+                        <div class="flex items-start justify-start my-6">
+                            <p class="text-sm font-bold">Total ({{ players.total }})</p>
+                        </div>
+                        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+                            <template v-for="player in players.data" :key="player.id">
+                                <MainPlayerCard :player="player" />
+                            </template>
+                        </div>
+                        <div class="flex items-center justify-center my-4">
+                            <Pagination :links="players.links" />
+                        </div>
                     </div>
-
-                    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-                        <template v-for="player in players.data" :key="player.id">
-                            <MainPlayerCard :player="player" />
-                        </template>
-                    </div>
-
-                    <div class="flex items-center justify-center my-4">
-                        <Pagination :links="players.links" />
-                    </div>
+                </div>
+                <div v-show="currentTabId == 2" class="w-full overflow-hidden bg-white shadow-xl sm:rounded-lg ">
+                    <PlayersMap :players="players" />
                 </div>
 
                 <!-- Filters Modal...
-                                                                                                    =====================================================-->
+                                                                                                                                                                                                                                                =====================================================-->
                 <div class="fixed bottom-0 right-0 p-5 sm:px-10 lg:px-20">
                     <button class="flex items-center justify-center w-16 h-16 text-center bg-black rounded-full shadow-xl"
                         @click="showFiltersModal = !showFiltersModal">
@@ -166,7 +184,7 @@ const options = {
                             <form @submit.prevent="filter">
                                 <div class="my-6">
                                     <InputLabel>Age</InputLabel>
-                                    <div class="px-4 py-2 mx-4 border border-white  rounded-full">
+                                    <div class="px-4 py-2 mx-4 border border-white rounded-full">
                                         <el-slider v-model="form.age" class="" :min="18" :max="70" />
                                     </div>
                                 </div>
