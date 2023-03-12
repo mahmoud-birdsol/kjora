f94bf296a259d18300f4aab0c5aab63142b372fb
aml walaed <amlwalaed@gmail.com>
2023-03-12 11:33:28 +0200

    some updates to the UI

--- resources/js/Pages/Upgrade.vue
+++ resources/js/Pages/Upgrade.vue
@@ -12,6 +12,8 @@ import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 import { CheckIcon, StarIcon  } from '@heroicons/vue/24/outline';
 import { CheckCircleIcon } from  "@heroicons/vue/20/solid"
 import PrimaryButton from '@/Components/PrimaryButton.vue'
+import FadeInTransition from "@/Components/FadeInTransition.vue";
+import { computed} from 'vue'
 let form = useForm({
     payment_plan: null
 })
@@ -25,7 +27,10 @@ let month = date.getMonth() ;
 let year = date.getFullYear();
 let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 const currentDate = `${day} ${months[month]} ${year}`
-const expaireDate = `${day} ${months[month+3]} ${year}`
+let expaireDate = computed(()=>{
+    if(form.payment_plan == 'yearly') return `${day} ${months[month+1]} ${year}`
+    else  return `${day} ${months[month]} ${year+1}`
+})
 </script>
 <template>
     <Head title="upgrade" />
@@ -60,7 +65,7 @@ const expaireDate = `${day} ${months[month+3]} ${year}`
                                 <span>Lorem ipsum dolor sit amet </span>
                             </li>
                         </ul>
-                        <div class="text-gray-600 text-sm font-bold my-3 ">CHOOSE PLAN</div>
+                        <div class="text-gray-600 text-sm font-bold my-3">CHOOSE PLAN</div>
                         <RadioGroup v-model="form.payment_plan" class="flex flex-col gap-2">
                             <RadioGroupOption v-slot="{ checked }" value="monthly"
                                 class="[&_li]:py-3 [&_li]:pl-6 [&_li]:pr-3 [&_li]:rounded-full [&_li]:border-2 [&_li]:list-none text-stone-500  flex flex-col  text-sm font-medium cursor-pointer">
@@ -79,7 +84,7 @@ const expaireDate = `${day} ${months[month+3]} ${year}`
                                 </li>
                             </RadioGroupOption>
                         </RadioGroup>
-                        <PrimaryButton class="my-3" @click="send">upgrade</PrimaryButton>
+                        <PrimaryButton class="my-3" @click="send" :disabled="!form.payment_plan">upgrade</PrimaryButton>
                     </template>
                 </CardContent>
             </Card>
@@ -89,17 +94,22 @@ const expaireDate = `${day} ${months[month+3]} ${year}`
                         <h1 class="text-7xl">subscription</h1>
                     </template>
                     <template #body>
+                        <FadeInTransition
+                        enter-from-class="opacity-0"
+                        enter-to-class="opacity-100"
+                        enter-active-class="transition-all duration-200"
+                        >
                             <div class="relative  rounded-xl mx-auto w-full bg-no-repeat bg-cover max-w-sm text-sm text-black"
+                            v-if="form.payment_plan==='monthly'"
                             >
                                 <img src="/images/player_bg_sm_gold.png" class="absolute top-0 left-0 w-full h-full" />
                                 <div class="bg-black rounded-full absolute top-0 right-0 m-4">
                                     <StarIcon class="w-4 h-4 fill-golden" />
                                 </div>
-                                <div class="p-8 font-bold z-10 relative">
+                                <div class="p-8 font-bold z-10 relative"> 
                                     <div class="mt-5 mb-16">
                                         <h2 class="uppercase text-primary" v-if="form.payment_plan=='monthly'">monthly</h2>
-                                        <h2 class="uppercase text-primary" v-else-if="form.payment_plan=='yearly'">yearly</h2>
-                                        <span v-text="form.payment_plan == 'monthly' ? '$10': '$25'"></span>
+                                        <span>10</span>
                                     </div>
                                     <div class="flex justify-between">
                                         <div>
@@ -113,6 +123,47 @@ const expaireDate = `${day} ${months[month+3]} ${year}`
                                     </div>
                                 </div>
                             </div>
+                            <div class="relative  rounded-xl mx-auto w-full bg-no-repeat bg-cover max-w-sm text-sm text-black"
+                            v-else-if="form.payment_plan==='yearly'"
+                            >
+                                <img src="/images/player_bg_sm_gold.png" class="absolute top-0 left-0 w-full h-full" />
+                                <div class="bg-black rounded-full absolute top-0 right-0 m-4">
+                                    <StarIcon class="w-4 h-4 fill-golden" />
+                                </div>
+                                <div class="p-8 font-bold z-10 relative"> 
+                                    <div class="mt-5 mb-16">
+                                        <h2 class="uppercase text-primary">yearly</h2>
+                                        <span>25$</span>
+                                    </div>
+                                    <div class="flex justify-between">
+                                        <div>
+                                            <div class="uppercase text-primary">start</div>
+                                            <div>{{ currentDate }}</div>
+                                        </div>
+                                        <div>
+                                            <div class="uppercase text-primary">end</div>
+                                            <div>{{ expaireDate }}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="relative mx-auto w-full bg-no-repeat bg-cover max-w-sm text-sm text-black"
+                            v-else
+                            >
+                                <img src="/images/player_bg_sm.png" class="absolute  rounded-xl top-0 left-0 w-full h-full" />
+                                <div class="bg-black rounded-full absolute top-0 right-0 m-4">
+                                    <StarIcon class="w-4 h-4 fill-primary" />
+                                </div>
+                                <div class="p-8 font-bold z-10 relative"> 
+                                    <div class="mt-5 mb-16">
+                                        <h2 class="uppercase text-white">free plan</h2>
+                                    </div>
+                                </div>
+                                <div class="flex justify-center py-6">
+                                            <div class="uppercase text-white opacity-50">minimal features</div>
+                                </div>
+                            </div>
+                        </FadeInTransition>
                     </template>
                 </CardContent>
             </Card>
