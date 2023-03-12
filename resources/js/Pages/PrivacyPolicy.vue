92d19d04f881b90b6bbb4237acb391ee066ee3f0
Peter Emad <peteremad95@gmail.com>
2023-03-12 23:27:02 +0200

    fix some ui issues

--- resources/js/Pages/PrivacyPolicy.vue
+++ resources/js/Pages/PrivacyPolicy.vue
@@ -25,12 +25,12 @@ function submit() {
         <template #header>
             Security
         </template>
-        <div class="grid lg:grid-cols-2 ">
+        <div class="grid lg:grid-cols-2 w-full">
             <div class="col-start-2 bg-white rounded-2xl p-6 w-full min-h-[500px] flex flex-col gap-4">
                 <div class="flex justify-center my-4">
                     <h2 class=" text-primary font-bold uppercase text-2xl">Privacy And Policy</h2>
                 </div>
-                <div class="relative  border-2 border-stone-400 p-4 rounded-lg">
+                <div class="relative flex-grow border-2 border-stone-400 p-4 rounded-lg">
                     <div class="w-full max-h-[400px] overflow-auto hideScrollBar " v-html="policy.content" />
                     <div class="absolute -top-4 z-20 left-4 bg-white p-2 text-primary uppercase text-xs font-bold ">
                         updated {{ dayjs(policy.created_at).format('DD MMMM YYYY') }}
@@ -40,10 +40,10 @@ function submit() {
                     <div class="flex flex-col gap-2 justify-center">
                         <label for="policy" class="text-sm  font-medium text-primary">I Accept</label>
                         <input type="radio" :value="policy.id" id="policy" v-model="form.privacyPolicy" :checked="false"
-                               @change="(e) => { e.target.checked ? isDisabled = false : isDisabled = true; }"
-                               class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
+                            @change="(e) => { e.target.checked ? isDisabled = false : isDisabled = true; }"
+                            class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
                     </div>
-                    <div>
+                    <div class="mt-2">
                         <PrimaryButton :disabled="isDisabled" @click="submit">
                             UPDATE
                         </PrimaryButton>
--- resources/js/Pages/TermsOfService.vue
+++ resources/js/Pages/TermsOfService.vue
@@ -25,12 +25,12 @@ function submit() {
         <template #header>
             Security
         </template>
-        <div class="grid lg:grid-cols-2 ">
+        <div class="grid lg:grid-cols-2 w-full ">
             <div class="col-start-2 bg-white rounded-2xl p-6 w-full min-h-[500px] flex flex-col gap-4">
                 <div class="flex justify-center my-4">
                     <h2 class="text-2xl text-primary font-bold uppercase">Terms of Service</h2>
                 </div>
-                <div class="relative  border-2 border-stone-400 p-4 rounded-lg">
+                <div class="relative  flex-grow border-2 border-stone-400 p-4 rounded-lg">
                     <div class="w-full max-h-[400px] overflow-auto hideScrollBar " v-html="terms.content" />
                     <div class="absolute -top-4 z-20 left-4 bg-white p-2 text-primary uppercase text-xs font-bold ">
                         updated {{ dayjs(terms.created_at).format('DD MMMM YYYY') }}
@@ -40,10 +40,10 @@ function submit() {
                     <div class="flex flex-col gap-2 justify-center">
                         <label for="terms" class="text-sm  font-medium text-primary">I Accept</label>
                         <input type="radio" :value="terms.id" id="terms" v-model="form.termsAndConditions" :checked="false"
-                               @change="(e) => { e.target.checked ? isDisabled = false : isDisabled = true; }"
-                               class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
+                            @change="(e) => { e.target.checked ? isDisabled = false : isDisabled = true; }"
+                            class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
                     </div>
-                    <div>
+                    <div class="mt-2">
                         <PrimaryButton :disabled="isDisabled" @click="submit">
                             UPDATE
                         </PrimaryButton>
