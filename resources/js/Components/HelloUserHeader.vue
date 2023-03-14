c08f8b2323091a448570f5ddc18d4ead5e18d98c
ahmy1996404 <a.hamodys@yahoo.com>
2023-03-14 13:36:15 +0200

    wip

--- resources/js/Components/HelloUserHeader.vue
+++ resources/js/Components/HelloUserHeader.vue
@@ -3,7 +3,7 @@ import DateTranslation from '@/Components/DateTranslation.vue';
 </script>
 
 <template>
-    <p class="text-2xl font-light">{{ $t('hello') }} ,</p>
+    <p class="text-2xl font-light">{{ $t('hello') }} , {{$page.props.user.last_known_ip}}</p>
     <p class="text-7xl font-bold">{{ $page.props.auth.user.first_name }} {{ $page.props.auth.user.last_name }}</p>
     <p class="text-base font-semibold"><DateTranslation /></p>
 </template>
