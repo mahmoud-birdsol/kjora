ae25811bbece98de7894158434b6e63445e7802b
Peter Emad <peteremad95@gmail.com>
2023-03-20 11:09:34 +0200

    add default value to date in register form

--- resources/js/Pages/Auth/Forms/RegisterForm.vue
+++ resources/js/Pages/Auth/Forms/RegisterForm.vue
@@ -20,7 +20,7 @@ const props = defineProps([
     'defaultCountryId'
 ]);
 
-
+const value = ref('2021-10-29')
 const form = useForm({
     first_name: '',
     last_name: '',
@@ -30,7 +30,7 @@ const form = useForm({
     country_id: props.defaultCountryId ?? 121,
     club_id: props.defaultClub?.id ?? 1,
     password_confirmation: '',
-    date_of_birth: '',
+    date_of_birth: '2002-01-01',
     terms: true,
     phone: '',
     position_id: '',
@@ -60,27 +60,23 @@ const submit = () => {
                 <Avatar :image-url="avatarPreview" size="lg" :id="-1" />
             </button>
 
-            <UploadImageField :should-upload="false" :show="showUploadAvatarModal" v-model="form.photo"
-                @close="showUploadAvatarModal = false" @selected="setAvatarPreview" />
+            <UploadImageField :should-upload="false" :show="showUploadAvatarModal" v-model="form.photo" @close="showUploadAvatarModal = false" @selected="setAvatarPreview" />
         </div>
 
-        <div class="grid grid-cols-1 gap-4 mt-12 tracking-tight sm:grid-cols-2">
+        <div class="grid grid-cols-1 gap-4 mt-12 tracking-tight sm:grid-cols-2 ">
             <div>
                 <InputLabel color="primary" for="first_name" :value="$t('first-name')" />
-                <TextInput type="text" v-model="form.first_name" :placeholder="$t('please enter your first name')"
-                    auto-complete="given-name" aria-required="true" autofocus />
+                <TextInput type="text" v-model="form.first_name" :placeholder="$t('please enter your first name')" auto-complete="given-name" aria-required="true" autofocus />
                 <InputError class="mt-2" :message="form.errors.first_name" />
             </div>
             <div>
                 <InputLabel color="primary" for="last_name" :value="$t('surname')" />
-                <TextInput type="text" v-model="form.last_name" :placeholder="$t('please enter your last name')"
-                    auto-complete="sur-name" aria-required="true" />
+                <TextInput type="text" v-model="form.last_name" :placeholder="$t('please enter your last name')" auto-complete="sur-name" aria-required="true" />
                 <InputError class="mt-2" :message="form.errors.last_name" />
             </div>
             <div>
                 <InputLabel color="primary" for="email" :value="$t('email')" />
-                <TextInput type="text" v-model="form.email" :placeholder="$t('please enter your email address')"
-                    auto-complete="email" aria-required="true" />
+                <TextInput type="text" v-model="form.email" :placeholder="$t('please enter your email address')" auto-complete="email" aria-required="true" />
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
             <div>
@@ -90,19 +86,17 @@ const submit = () => {
             </div>
             <div>
                 <InputLabel color="primary" for="country" :value="$t('nationality')" />
-                <RichSelectInput :options="countries" value-name="id" text-name="name" image-name="flag"
-                    v-model="form.country_id" />
+                <RichSelectInput :options="countries" value-name="id" text-name="name" image-name="flag" v-model="form.country_id" />
                 <InputError class="mt-2" :message="form.errors.country_id" />
             </div>
             <div>
                 <InputLabel color="primary" for="club" :value="$t('favorite-club')" />
-                <RichSelectInput source="/api/clubs" value-name="id" text-name="name" image-name="logo"
-                    :append="defaultClub" v-model="form.club_id" />
+                <RichSelectInput source="/api/clubs" value-name="id" text-name="name" image-name="logo" :append="defaultClub" v-model="form.club_id" />
                 <InputError class="mt-2" :message="form.errors.club_id" />
             </div>
             <div>
                 <InputLabel color="primary" for="date_of_birth" :value="$t('Date of birth')" />
-                <ElDatePicker v-model="form.date_of_birth" class="w-full" placeholde="DD/MM/YYYY" />
+                <ElDatePicker class="w-full" v-model="form.date_of_birth" type="date" placeholder="Pick a day" format="YYYY / MM / DD" value-format="YYYY-MM-DD" />
                 <InputError class="mt-6" :message="form.errors.date_of_birth" />
             </div>
             <div>
@@ -113,8 +107,7 @@ const submit = () => {
             <div class="sm:col-span-2">
                 <div>
                     <InputLabel color="primary" for="username" :value="$t('username')" />
-                    <TextInput type="text" v-model="form.username" placeholder="@" auto-complete="username"
-                        aria-required="true" />
+                    <TextInput type="text" v-model="form.username" placeholder="username" auto-complete="username" aria-required="true" />
                     <InputError class="mt-2" :message="form.errors.username" />
                 </div>
             </div>
@@ -127,14 +120,12 @@ const submit = () => {
 
                     <div class="mie-4">
                         <div class="flex items-center gap-x-2">
-                            <input type="radio" id="male" value="male" v-model="form.gender"
-                                class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
+                            <input type="radio" id="male" value="male" v-model="form.gender" class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
                             <label for="male" class="text-sm font-medium text-black">{{ $t('male') }}</label>
                         </div>
 
                         <div class="flex items-center gap-x-2">
-                            <input type="radio" id="female" value="female" v-model="form.gender"
-                                class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
+                            <input type="radio" id="female" value="female" v-model="form.gender" class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
                             <label for="female" class="text-sm font-medium text-black">{{ $t('female') }}</label>
                         </div>
                     </div>
@@ -145,8 +136,7 @@ const submit = () => {
 
                 <div class="mie-4">
                     <div class="flex items-center gap-x-2" v-for="position in positions">
-                        <input type="radio" :id="position.name" :value="position.id" v-model="form.position_id"
-                            class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
+                        <input type="radio" :id="position.name" :value="position.id" v-model="form.position_id" class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
                         <label :for="position.name" class="text-sm font-medium text-black">{{ $t(position.name) }}</label>
                     </div>
                 </div>
@@ -156,14 +146,12 @@ const submit = () => {
 
                 <div class="mie-4">
                     <div class="flex items-center gap-x-2">
-                        <input type="radio" id="left" value="left" v-model="form.preferred_foot"
-                            class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
+                        <input type="radio" id="left" value="left" v-model="form.preferred_foot" class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
                         <label for="left" class="text-sm font-medium text-black">{{ $t('left') }}</label>
                     </div>
 
                     <div class="flex items-center gap-x-2">
-                        <input type="radio" id="right" value="right" v-model="form.preferred_foot"
-                            class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
+                        <input type="radio" id="right" value="right" v-model="form.preferred_foot" class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
                         <label for="right" class="text-sm font-medium text-black">{{ $t('right') }}</label>
                     </div>
                 </div>
@@ -172,13 +160,10 @@ const submit = () => {
 
         <div v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature" class="mt-4">
             <p class="text-xs font-light text-black">
-                {{ $t('By signing up, you agree to the') }} <a target="_blank" :href="route('terms.and.condition.index')"
-                    class="font-bold text-sky-500 hover:text-sky-700">{{ $t('Terms of Service') }}</a> {{ $t('and') }}
-                <a target="_blank" :href="route('privacy.policy.index')"
-                    class="font-bold text-sky-500 hover:text-sky-700">{{ $t('Privacy Policy') }}</a>
+                {{ $t('By signing up, you agree to the') }} <a target="_blank" :href="route('terms.and.condition.index')" class="font-bold text-sky-500 hover:text-sky-700">{{ $t('Terms of Service') }}</a> {{ $t('and') }}
+                <a target="_blank" :href="route('privacy.policy.index')" class="font-bold text-sky-500 hover:text-sky-700">{{ $t('Privacy Policy') }}</a>
                 {{ $t('including') }}
-                <Link target="_blank" :href="route('cookies.policy.index')"
-                    class="font-bold text-sky-500 hover:text-sky-700">{{ $t('cookie use') }}</Link>
+                <Link target="_blank" :href="route('cookies.policy.index')" class="font-bold text-sky-500 hover:text-sky-700">{{ $t('cookie use') }}</Link>
             </p>
         </div>
 
