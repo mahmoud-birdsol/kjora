4bb7fb6bfe105633e07d84bb9bf40c6bcfc74fbf
Peter Emad <peteremad95@gmail.com>
2023-03-20 14:52:52 +0200

    make notifiation disappear after 10 seconds

--- resources/js/Layouts/Partials/RealtimeNotifications.vue
+++ resources/js/Layouts/Partials/RealtimeNotifications.vue
@@ -1,7 +1,7 @@
 <script setup>
-import {ref} from 'vue';
-import {usePage} from '@inertiajs/inertia-vue3';
-import {Inertia} from '@inertiajs/inertia';
+import { onMounted, ref } from 'vue';
+import { usePage } from '@inertiajs/inertia-vue3';
+import { Inertia } from '@inertiajs/inertia';
 import SimpleNotification from '@/Components/Notifications/SimpleNotification.vue';
 import RespondToInvitationModal from '@/Components/RespondToInvitationModal.vue';
 import UserNotification from "@/Components/Notifications/UserNotification.vue";
@@ -24,46 +24,35 @@ Echo.private('users.' + usePage().props.value.auth.user.id)
         Inertia.reload({
             only: ['notifications'],
         });
+        setTimeout(() => {
+            showNotificationPopup.value = false
+        }, 10000)
     });
 </script>
 
 <template>
-    <div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
-        <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
-            <transition enter-active-class="transform ease-out duration-300 transition"
-                        enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-                        enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
-                        leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
-                        leave-to-class="opacity-0">
+    <div aria-live="assertive" class="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:items-start sm:p-6">
+        <div class="flex flex-col items-center w-full space-y-4 sm:items-end">
+            <transition enter-active-class="transition duration-300 ease-out transform" enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2" enter-to-class="translate-y-0 opacity-100 sm:translate-x-0" leave-active-class="transition duration-100 ease-in"
+                leave-from-class="opacity-100" leave-to-class="opacity-0">
                 <template v-if="showNotificationPopup && broadcastNotification.displayType == 'simple'">
-                    <SimpleNotification
-                        :notification="broadcastNotification"
-                        @close="showNotificationPopup = false"/>
+                    <SimpleNotification :notification="broadcastNotification" @close="showNotificationPopup = false" />
                 </template>
             </transition>
 
-            <transition enter-active-class="transform ease-out duration-300 transition"
-                        enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-                        enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
-                        leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
-                        leave-to-class="opacity-0">
+            <transition enter-active-class="transition duration-300 ease-out transform" enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2" enter-to-class="translate-y-0 opacity-100 sm:translate-x-0" leave-active-class="transition duration-100 ease-in"
+                leave-from-class="opacity-100" leave-to-class="opacity-0">
                 <template v-if="showNotificationPopup && broadcastNotification.displayType == 'user'">
-                    <UserNotification
-                        :notification="broadcastNotification"
-                        @close="showNotificationPopup = false"/>
+                    <UserNotification :notification="broadcastNotification" @close="showNotificationPopup = false" />
                 </template>
             </transition>
         </div>
     </div>
 
-    <transition enter-active-class="transform ease-out duration-300 transition"
-                enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-                enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
-                leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
-                leave-to-class="opacity-0">
+    <transition enter-active-class="transition duration-300 ease-out transform" enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2" enter-to-class="translate-y-0 opacity-100 sm:translate-x-0" leave-active-class="transition duration-100 ease-in" leave-from-class="opacity-100"
+        leave-to-class="opacity-0">
         <div v-if="showInvitationNotificationModal">
-            <RespondToInvitationModal :invitation="invitation" :show="showInvitationNotificationModal"
-                                      @close="showInvitationNotificationModal = false"/>
+            <RespondToInvitationModal :invitation="invitation" :show="showInvitationNotificationModal" @close="showInvitationNotificationModal = false" />
         </div>
     </transition>
 </template>
