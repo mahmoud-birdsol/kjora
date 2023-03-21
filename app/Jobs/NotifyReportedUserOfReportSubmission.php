24ad9041cac8113f7d9da66de8af5dbf82aa96bd
ahmy1996404 <a.hamodys@yahoo.com>
2023-03-21 18:17:02 +0200

    report notification

---
+++ app/Jobs/NotifyReportedUserOfReportSubmission.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Admin;
+use App\Models\Report;
+use App\Notifications\ReportSubmittedNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class NotifyReportedUserOfReportSubmission implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private Report $report;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Report $report)
+    {
+        $this->report = $report;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+
+        $this->report->reportable->reportedUser()->each->notify(new ReportSubmittedNotification($this->report));
+    }
+}
