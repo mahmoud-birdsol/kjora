68f22fd696cd1a104653682967e9b21eba77c227
aml walaed <amlwalaed@gmail.com>
2023-03-21 11:47:11 +0200

    resize name in small screen

--- resources/js/Components/HelloUserHeader.vue
+++ resources/js/Components/HelloUserHeader.vue
@@ -4,6 +4,6 @@ import DateTranslation from '@/Components/DateTranslation.vue';
 
 <template>
     <p class="text-2xl font-light">{{ $t('hello') }} ,</p>
-    <p class="text-7xl font-bold">{{ $page.props.auth.user.first_name }} {{ $page.props.auth.user.last_name }}</p>
+    <p class="text-3xl max-sm:whitespace-nowrap md:text-7xl font-bold">ahmed el sheridah</p>
     <p class="text-base font-semibold"><DateTranslation /></p>
 </template>
