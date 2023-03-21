018a1dee59113c8a9f31ab44f2324d364fe51797
Peter Emad <peteremad95@gmail.com>
2023-03-21 18:27:18 +0200

    wip

--- resources/js/Pages/CookiesPolicy.vue
+++ resources/js/Pages/CookiesPolicy.vue
@@ -21,35 +21,33 @@ function submit() {
 </script>
 
 <template>
-    <Head title="Cookie use"/>
+    <Head title="Cookie use" />
     <GuestLayout title="Cookie use">
         <template #header>
-            {{$t('Security')}}
+            {{ $t('Security') }}
         </template>
         <div class="grid w-full lg:grid-cols-2 ">
             <div class="col-start-2 bg-white rounded-2xl p-6 w-full min-h-[500px] flex flex-col gap-4">
                 <div class="flex justify-center my-4">
-                    <h2 class="text-2xl font-bold uppercase text-primary">{{$t('cookie use')}}</h2>
+                    <h2 class="text-2xl font-bold uppercase text-primary">{{ $t('cookie use') }}</h2>
                 </div>
                 <div v-if="cookies" class="relative flex-grow p-4 border-2 rounded-lg border-stone-400">
                     <div class="w-full max-h-[400px] overflow-auto hideScrollBar " v-html="cookies.content" />
 
                     <div class="absolute z-20 p-2 text-xs font-bold uppercase bg-white -top-4 left-4 text-primary ">
-                        {{$t('updated')}} <DateTranslation format="DD MMMM YYYY" :start="cookies.created_at"/>
+                        {{ $t('updated') }}
+                        <DateTranslation format="DD MMMM YYYY" :start="cookies.created_at" />
 
                     </div>
                 </div>
-                <div class=""
-                    v-if="$page.props.user && cookies && (cookies.version !== $page.props.auth.user.accepted_cookie_policy_version)">
+                <div class="" v-if="$page.props.user && cookies && (cookies.version !== $page.props.auth.user.accepted_cookie_policy_version)">
                     <div class="flex flex-col justify-center mt-4 gap-2">
-                        <label for="cookies" class="text-sm font-medium text-primary">I Accept</label>
-                        <input type="radio" :value="cookies.id" id="cookies" v-model="form.cookiePolicy" :checked="false"
-                            @change="(e) => { e.target.checked ? isDisabled = false : isDisabled = true; }"
-                            class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
+                        <label for="cookies" class="text-sm font-medium text-primary">{{ $t('I agree') }}</label>
+                        <input type="radio" :value="cookies.id" id="cookies" v-model="form.cookiePolicy" :checked="false" @change="(e) => { e.target.checked ? isDisabled = false : isDisabled = true; }" class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
                     </div>
                     <div class="mt-2">
                         <PrimaryButton :disabled="isDisabled" @click="submit">
-                            {{$t('UPDATE')}}
+                            {{ $t('UPDATE') }}
                         </PrimaryButton>
                     </div>
                 </div>
--- resources/js/Pages/PrivacyPolicy.vue
+++ resources/js/Pages/PrivacyPolicy.vue
@@ -23,32 +23,30 @@ function submit() {
 <template>
     <GuestLayout title="Privacy policy">
         <template #header>
-            {{$t('Security')}}
+            {{ $t('Security') }}
         </template>
         <div class="grid w-full lg:grid-cols-2">
             <div class="col-start-2 bg-white rounded-2xl p-6 w-full min-h-[500px] flex flex-col gap-4">
                 <div class="flex justify-center my-4">
 
-                    <h2 class="text-2xl font-bold uppercase text-primary">{{$t('Privacy And Policy')}}</h2>
+                    <h2 class="text-2xl font-bold uppercase text-primary">{{ $t('Privacy And Policy') }}</h2>
                 </div>
                 <div v-if="policy" class="relative flex-grow p-4 border-2 rounded-lg border-stone-400">
                     <div class="w-full max-h-[400px] overflow-auto hideScrollBar " v-html="policy.content" />
 
                     <div class="absolute z-20 p-2 text-xs font-bold uppercase bg-white -top-4 left-4 text-primary ">
-                        {{$t('updated')}} <DateTranslation format="DD MMMM YYYY" :start="policy.created_at"/>
+                        {{ $t('updated') }}
+                        <DateTranslation format="DD MMMM YYYY" :start="policy.created_at" />
                     </div>
                 </div>
-                <div class=""
-                    v-if="$page.props.user && policy && (policy.version !== $page.props.auth.user.accepted_privacy_policy_version)">
+                <div class="" v-if="$page.props.user && policy && (policy.version !== $page.props.auth.user.accepted_privacy_policy_version)">
                     <div class="flex flex-col justify-center gap-2">
-                        <label for="policy" class="text-sm font-medium text-primary">I Accept</label>
-                        <input type="radio" :value="policy.id" id="policy" v-model="form.privacyPolicy" :checked="false"
-                            @change="(e) => { e.target.checked ? isDisabled = false : isDisabled = true; }"
-                            class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
+                        <label for="policy" class="text-sm font-medium text-primary">{{ $t('I agree') }}</label>
+                        <input type="radio" :value="policy.id" id="policy" v-model="form.privacyPolicy" :checked="false" @change="(e) => { e.target.checked ? isDisabled = false : isDisabled = true; }" class="accent-primary checked:bg-primary focus:bg-primary focus:ring-primary" />
                     </div>
                     <div class="mt-2">
                         <PrimaryButton :disabled="isDisabled" @click="submit">
-                            {{$t('UPDATE')}}
+                            {{ $t('UPDATE') }}
                         </PrimaryButton>
                     </div>
                 </div>
