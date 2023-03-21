ed0b33aaaf907cd81a8ffafe03bece76bc51e13c
Peter Emad <peteremad95@gmail.com>
2023-03-21 12:24:43 +0200

    wip share profile feature

--- resources/js/Components/Socials.vue
+++ resources/js/Components/Socials.vue
@@ -20,7 +20,7 @@ function copy() {
     ) => {
         show.value = true
 
-        setTimeout(() => { show.value = false }, 1000)
+        setTimeout(() => { show.value = false }, 3000)
     })
 }
 </script>
@@ -29,7 +29,7 @@ function copy() {
     <div class="relative mt-2">
         <Transition enter-from-class="scale-0" enter-to-class="scale-100" enter-active-class="transition-all duration-300" leave-to-class="scale-0" leave-active-class="transition-all duration-300">
             <div v-if="showSocials" class="bg-black bg-opacity-70 rounded-full p-1 flex gap-3 absolute -top-full ltr:right-1/2 rtl:left-1/2 z-30">
-                <a :href="'https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=' + url" target="_blank">
+                <a :data-href="'https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=' + url" :href="'https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=' + url" target="_blank">
                     <Facebook class="h-4 w-4 " />
                 </a>
                 <a :href="'https://twitter.com/intent/tweet?hashtags=kjora&original_referer=' + url" target="_blank">
@@ -41,7 +41,7 @@ function copy() {
                 </a>
                 <div class="relative">
                     <LinkIcon class="h-4 w-4 text-white" @click="copy" />
-                    <span class="bg-white text-black text-[8px] font-bold rounded absolute bottom-full p-1" v-if="show">{{ $t('copied') }}!</span>
+                    <span class="bg-white text-black text-[8px] font-bold rounded absolute ltr:right-0 rtl:left-0 bottom-[110%] p-1" v-if="show">{{ $t('copied') }}!</span>
                 </div>
             </div>
         </Transition>
