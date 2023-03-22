6371276ca19c9c56b8502a0607c94e1bf4909ad3
Peter Emad <peteremad95@gmail.com>
2023-03-22 11:02:16 +0200

    invitation fileter changes

--- resources/js/Components/InvitationsFilter.vue
+++ resources/js/Components/InvitationsFilter.vue
@@ -42,49 +42,42 @@ const reset = () => {
 </script>
 <template>
     <FixedWrapper>
-        <button class="flex items-center justify-center w-16 h-16 text-center bg-black rounded-full shadow-xl"
-            @click="showFiltersModal = !showFiltersModal">
+        <button class="flex items-center justify-center w-16 h-16 text-center bg-black rounded-full shadow-xl" @click="showFiltersModal = !showFiltersModal">
             <AdjustmentsHorizontalIcon class="w-10 h-10 text-white" />
         </button>
         <Modal :show="showFiltersModal" max-width="sm" @close="showFiltersModal = false" :closeable="false">
-            <div class="p-6 bg-black" v-loading="loading">
-                <div class="grid">
-                    <button @click="showFiltersModal = false" class="justify-self-end">
-                        <XMarkIcon class="w-4 h-4 text-white" />
-                    </button>
-                    <p class="text-sm text-white">{{$t('filter')}} </p>
-    
-                </div>
-    
-                <form @submit.prevent="filter">
-                    <div class="my-6">
-                        <InputLabel>{{$t('date from')}}: </InputLabel>
-                        <ElDatePicker v-model="form.dateFrom" class="w-full" type="datetime" :placeholder="$t('pick a date')"
-                            format="YYYY/MM/DD hh:mm:ss" value-format="YYYY/MM/DD HH:mm:ss" />
-                    </div>
-                    <div class="my-6">
-                        <InputLabel>{{$t('date to') }}:</InputLabel>
-                        <ElDatePicker v-model="form.dateTo" class="w-full" type="datetime" :placeholder="$t('pick a date')"
-                            format="YYYY/MM/DD hh:mm:ss" value-format="YYYY/MM/DD HH:mm:ss" />
-                    </div>
-                    <div class="my-6">
-                        <InputLabel>{{$t('search')}}</InputLabel>
-                        <div>
-                            <input type="search" name="search" id="search" v-model="form.search"
-                                class="block w-full text-center text-white bg-black border-white rounded-full focus:border-primary focus:ring-primary sm:text-sm placeholder:center"
-                                :placeholder="$t('search by name or username')" />
-                        </div>
-                    </div>
-                    <div class="my-6 mt-4">
-                        <SecondaryButton @click="filter">{{$t('apply')}}</SecondaryButton>
-                    </div>
-                    <div class="flex items-center justify-center mt-4">
+            <div class=" bg-black" v-loading="loading">
+                <button @click="showFiltersModal = false" class="justify-self-end p-1">
+                    <XMarkIcon class="w-4 h-4 text-white" />
+                </button>
+                <div class="p-6">
+                    <div class="flex gap-2 items-center justify-between">
+                        <p class="text-sm text-white rtl:text-start  ">{{ $t('filter') }} </p>
                         <button class="text-primary" @click="reset">
                             <!-- <XMarkIcon class="inline w-4 h-4 mr-4" /> -->
-                            {{$t('reset')}}
+                            {{ $t('reset') }}
                         </button>
                     </div>
-                </form>
+                    <form @submit.prevent="filter" class="rtl:text-start">
+                        <div class="my-6">
+                            <InputLabel>{{ $t('date from') }}: </InputLabel>
+                            <ElDatePicker v-model="form.dateFrom" class="w-full" type="datetime" :placeholder="$t('date')" format="YYYY/MM/DD hh:mm:ss" value-format="YYYY/MM/DD HH:mm:ss" />
+                        </div>
+                        <div class="my-6">
+                            <InputLabel>{{ $t('date to') }}:</InputLabel>
+                            <ElDatePicker v-model="form.dateTo" class="w-full" type="datetime" :placeholder="$t('date')" format="YYYY/MM/DD hh:mm:ss" value-format="YYYY/MM/DD HH:mm:ss" />
+                        </div>
+                        <div class="my-6">
+                            <InputLabel>{{ $t('search') }}</InputLabel>
+                            <div>
+                                <input type="search" name="search" id="search" v-model="form.search" class="block w-full text-center text-white bg-black border-white rounded-full focus:border-primary focus:ring-primary sm:text-sm placeholder:center" :placeholder="$t('search by name or username')" />
+                            </div>
+                        </div>
+                        <div class="my-6 mt-4">
+                            <SecondaryButton @click="filter">{{ $t('apply') }}</SecondaryButton>
+                        </div>
+                    </form>
+                </div>
             </div>
         </Modal>
     </FixedWrapper>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,9 +14,7 @@
     {{-- <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">--}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;700&display=swap" rel="stylesheet">
 
     <!-- Scripts -->
@@ -25,7 +23,8 @@
     @inertiaHead
 </head>
 
-<body class="font-sans antialiased">
-@inertia
+<body class="font-sans antialiased" dir="{{app()->getLocale() == 'ar' ? 'rtl' : 'ltr'}}">
+    @inertia
 </body>
+
 </html>
