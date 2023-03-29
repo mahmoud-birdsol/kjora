db3c28a4ab7796a821607782697861748d2c717c
Peter Emad <peteremad95@gmail.com>
2023-03-29 13:06:31 +0200

    wip

--- resources/js/Pages/Invitation/Index.vue
+++ resources/js/Pages/Invitation/Index.vue
@@ -10,6 +10,7 @@ import { CalendarIcon } from "@heroicons/vue/20/solid";
 import dayjs from 'dayjs';
 import { computed, ref } from 'vue';
 import { paginationEmits } from 'element-plus';
+import InvitationHireTaps from '../../Components/InvitationHireTaps.vue';
 const props = defineProps({
     invitations: Array,
 });
@@ -48,27 +49,7 @@ function showFromToDates(date1, date2) {
 
         <div class="py-12">
             <div class="">
-                <div class="flex items-center justify-end gap-3">
-                    <Link :href="route('invitation.index')">
-                    <button
-                        class="py-2 px-4 min-w-[120px] sm:min-w-[215px]  font-bold  text-center items-center bg-white border-2 border-gray-300 rounded-full text-xs  text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-primary active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition whitespace-nowrap"
-                        :class="{ 'border-primary': route().current('invitation.index'), 'border-none': !route().current('invitation.index') }">
-
-                        <span :class="{ 'text-gray-500': !route().current('invitation.index'), 'text-black': route().current('invitation.index') }">
-                            {{ $t('invitation') }}
-                        </span>
-                    </button>
-                    </Link>
-                    <Link :href="route('hire.index')">
-                    <button
-                        class="py-2 px-4 min-w-[120px] sm:min-w-[215px]  font-bold  text-center items-center bg-white border-2 border-gray-300 rounded-full text-xs  text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-primary active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition whitespace-nowrap"
-                        :class="{ 'border-primary': route().current('hire.index'), 'border-none': !route().current('hire.index') }">
-                        <span :class="{ 'text-gray-500': !route().current('hire.index'), 'text-black': route().current('hire.index') }">
-                            {{ $t('hire') }}
-                        </span>
-                    </button>
-                    </Link>
-                </div>
+                <InvitationHireTaps />
 
                 <div class="bg-white rounded-xl mt-4 min-h-[500px] p-2 md:p-6">
                     <div class="flex gap-1 mb-4 text-xs font-bold" v-if="fromDate && toDate">
