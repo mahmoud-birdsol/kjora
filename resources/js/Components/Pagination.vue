7a534cc81bc44643d589e577e5a599a923c69961
Peter Emad <peteremad95@gmail.com>
2023-03-29 14:09:42 +0200

    wip

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -9,11 +9,8 @@ defineProps({
     <div v-if="links.length > 3">
         <div class="flex flex-wrap -mb-1">
             <template v-for="(link, p) in links" :key="p">
-                <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
-                     v-html="link.label"/>
-                <Link v-else
-                      class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white focus:border-primary focus:text-primary"
-                      :class="{ 'bg-primary text-white': link.active }" :href="link.url" v-html="link.label"/>
+                <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded" v-html="link.label" />
+                <Link v-else class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white hover:text-primary hover:border-primary focus:border-primary focus:text-primary" :class="{ 'bg-primary text-white': link.active }" :href="link.url" v-html="link.label" />
             </template>
         </div>
     </div>
