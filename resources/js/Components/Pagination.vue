53ca2e6c2f27eaea7c509745fd42e883903a8d95
Peter Emad <peteremad95@gmail.com>
2023-03-30 11:37:07 +0200

    change the pagination prev and next buttons

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -8,10 +8,14 @@ defineProps({
 <template>
     <div v-if="links.length > 3">
         <div class="flex flex-wrap -mb-1">
-            <template v-for="(link, p) in links" :key="p">
-                <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded" v-html="link.label" />
-                <Link v-else class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white hover:text-primary hover:border-primary focus:border-primary focus:text-primary" :class="{ 'bg-primary text-white': link.active }" :href="link.url" v-html="link.label" />
+            <div v-if="links.at(0).url === null" class="px-4 py-3 mb-1 mr-1 text-lg leading-4 text-gray-400 border rounded " v-html="'&laquo;'" />
+            <Link v-else class="px-4 py-3 mb-1 mr-1 text-lg leading-4 border rounded  hover:bg-white hover:text-primary hover:border-primary focus:border-primary focus:text-primary" :class="{ 'bg-primary text-white': links.at(0).active }" :href="links.at(0).url" v-html="'&laquo;'" />
+            <template v-for="(link, p) in links.slice(1, -1)" :key="p">
+                <div v-if="link.url === null" class="px-4 py-3 mb-1 mr-1 text-sm leading-4 text-gray-400 border rounded" v-html="link.label" />
+                <Link v-else class="px-4 py-3 mb-1 mr-1 text-sm leading-4 border rounded hover:bg-white hover:text-primary hover:border-primary focus:border-primary focus:text-primary" :class="{ 'bg-primary text-white': link.active }" :href="link.url" v-html="link.label" />
             </template>
+            <div v-if="links.at(-1).url === null" class="px-4 py-3 mb-1 mr-1 text-lg leading-4 text-gray-400 border rounded " v-html="'&raquo;'" />
+            <Link v-else class="px-4 py-3 mb-1 mr-1 text-lg leading-4 border rounded  hover:bg-white hover:text-primary hover:border-primary focus:border-primary focus:text-primary" :class="{ 'bg-primary text-white': links.at(0).active }" :href="links.at(-1).url" v-html="'&raquo;'" />
         </div>
     </div>
 </template>
