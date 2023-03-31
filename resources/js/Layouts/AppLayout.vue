5214c41e0440c78437b8f641945ab09036b99395
Peter Emad <peteremad95@gmail.com>
2023-03-31 19:49:50 +0200

    wip

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -49,7 +49,7 @@ const errorCallback = (error) => {
         <Head :title="title" />
 
         <div class="relative min-h-screen bg-gradient-to-b from-black to-primaryDark isolate before:-z-10"
-            :class="showBall ? ` before:bg-[url('../images/ballkjoura.png')] before:hidden sm:before:block before:absolute before:inset-0 before:bg-[center_-243px] before:bg-no-repeat before:mix-blend-overlay` : ''">
+            :class="showBall ? ` before:bg-[url('.././images/ballkjoura.png')] before:hidden sm:before:block before:absolute before:inset-0 before:bg-[center_-243px] before:bg-no-repeat before:mix-blend-overlay` : ''">
             <div class="flex flex-col justify-between min-h-screen pt-6 space-y-4 sm:pt-0 ltr:font-sans rtl:font-tahoma">
                 <Navbar />
 
