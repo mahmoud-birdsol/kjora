03de941936ea2f6ae4630f1baa89832e4fb0dba3
aml walaed <amlwalaed@gmail.com>
2023-04-05 09:12:50 +0200

    WIP

--- resources/js/Components/LikesModal.vue
+++ resources/js/Components/LikesModal.vue
@@ -37,22 +37,22 @@ const currentUser = usePage().props.value.auth.user
 <template>
     <Modal :show="show" :max-width="'xs'" :closeable="closeable" @close="close" :showCloseIcon="true">
         <div class="flex justify-center shadow-sm ">
-            <h2 class="text-lg font-bold flex gap-1  text-primary">
-                <span>{{ users.length }}</span>
-                <span class="transition-all duration-150 capitalize ">
-                    {{ users.length <= 1 ? $t('like') : $t('likes') }} </span>
+            <h2 class="flex gap-1 text-lg font-bold text-primary">
+                <span>{{ users?.length }}</span>
+                <span class="capitalize transition-all duration-150 ">
+                    {{ users?.length <= 1 ? $t('like') : $t('likes') }} </span>
             </h2>
         </div>
         <div class="min-h-[400px] max-h-[500px] p-4 pt-0 overflow-y-auto hideScrollBar">
 
             <template v-for="user in users" :key="user.id">
-                <div class="flex w-full items-center gap-2  p-2 text-stone-800 ">
+                <div class="flex items-center w-full gap-2 p-2 text-stone-800 ">
                     <div>
                         <Avatar :id="user.id" :username="user.name" :image-url="user.avatar_url" :size="'md'" :border="true" border-color="primary" />
                     </div>
                     <div class="flex flex-col">
                         <Link :href="route('player.profile', user.id)" class="flex flex-row gap-2">
-                        <h3 class="m-0  font-bold leading-none capitalize ">{{ user.name
+                        <h3 class="m-0 font-bold leading-none capitalize ">{{ user.name
                         }} </h3>
 
                         </Link>
