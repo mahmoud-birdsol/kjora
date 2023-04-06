8eca2968a076de263def336d3290b0d1e166f15f
aml walaed <amlwalaed@gmail.com>
2023-04-06 11:25:01 +0200

    make resize function composable hook

--- resources/js/Components/HelloUserHeader.vue
+++ resources/js/Components/HelloUserHeader.vue
@@ -1,29 +1,28 @@
 <script setup>
 import DateTranslation from '@/Components/DateTranslation.vue';
 import { inject, onMounted , onUnmounted, ref } from 'vue';
-
+import { useResizeFont } from '@/Composables/useResizeFont';
 const greetings = inject('greetings')
 
-function resize_to_fit(name ,container) {
-    if(!name || !container  ) return;
-    let fontSize = window.getComputedStyle(name).fontSize;
-    if (name.clientWidth >= container.clientWidth) {
-        name.style.fontSize = (parseFloat(fontSize) - 1) + 'px';
-        resize_to_fit(name , container)
-    }
-}
+// function resize_to_fit(name ,container) {
+//     if(!name || !container  ) return;
+//     let fontSize = window.getComputedStyle(name).fontSize;
+//     if (name.clientWidth >= container.clientWidth) {
+//         name.style.fontSize = (parseFloat(fontSize) - 1) + 'px';
+//         resize_to_fit(name , container)
+//     }
+// }
 let userName ,container ,fontSize;
+let { resizeFont } = useResizeFont()
 const changeFontSize =(e)=>{
         userName.style.fontSize=fontSize
-        resize_to_fit(userName , container)
-    }
+        resizeFont(userName , container)
+}
 onMounted(() => {
     userName = document.querySelector('.name');
     container = document.querySelector('.container');
     fontSize = window.getComputedStyle(userName).fontSize;
-    resize_to_fit(userName , container)
-
-   
+    resizeFont(userName , container)
     window.addEventListener('resize',changeFontSize)
 })
 onUnmounted(()=>{
@@ -34,7 +33,7 @@ onUnmounted(()=>{
 <template>
     <p class="text-2xl font-light">{{ greetings[$page.props.locale] ??  $t('hello') }} ,</p>
     <div class="container flex items-center w-[calc(100vw-3rem)] md:w-full">
-        <p class="font-bold name whitespace-nowrap text-7xl">{{ $page.props.auth.user.name }}</p>
+        <p class="font-bold name whitespace-nowrap text-7xl">{{ $page.props.auth.user.name }} sddjsnnjs sdnjs</p>
     </div>
     <p class="text-base font-semibold">
         <DateTranslation />
