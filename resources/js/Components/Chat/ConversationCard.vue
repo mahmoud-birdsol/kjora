14a467cde323c585916580c45e374217b3701938
Peter Emad <peteremad95@gmail.com>
2023-04-06 13:08:40 +0200

    wip

--- resources/js/Components/Chat/ConversationCard.vue
+++ resources/js/Components/Chat/ConversationCard.vue
@@ -49,7 +49,6 @@ function removeConversation() {
                 <span class="text-xs leading-none text-neutral-400 rtl:before:content-['a'] rtl:before:text-transparent"> @{{
                     user.username }} </span>
                 <div class="flex gap-1 sm:hidden " v-if="!user.online">
-                    <!-- <p class="text-xs text-gray-300">{{ $t('Last seen') }}</p> -->
                     <p class="text-xs text-gray-300">
                         <DateTranslation :end="user.last_seen_at" type="period" />
                     </p>
@@ -57,7 +56,6 @@ function removeConversation() {
             </div>
             <div class="flex gap-2 -mt-2 mis-auto">
                 <div class="max-sm:hidden " v-if="!user.online">
-                    <!-- <p class="text-xs text-gray-300 max-sm:scale-75">{{ $t('Last seen') }}</p> -->
                     <p class="text-xs text-gray-300 max-sm:scale-75">
                         <DateTranslation :end="user.last_seen_at" type="period" />
                     </p>
