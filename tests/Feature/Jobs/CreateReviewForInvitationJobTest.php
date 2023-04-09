2b68f7dfea3829bfb41397b52bc4d2e89f134d32
Osama Bassam <osama.m.bassam@gmail.com>
2023-04-09 14:51:41 +0200

    wip

---
+++ tests/Feature/Jobs/CreateReviewForInvitationJobTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Jobs;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateReviewForInvitationJobTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+//    public function ()
+//    {
+//
+//    }
+}
