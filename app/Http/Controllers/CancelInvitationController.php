a56667ee65b2c4d73f504f572396d40d6cdb0817
Peter Emad <peteremad95@gmail.com>
2023-05-02 11:13:41 +0300

    wip

--- app/Http/Controllers/CancelInvitationController.php
+++ app/Http/Controllers/CancelInvitationController.php
@@ -12,12 +12,12 @@ class CancelInvitationController extends Controller
     /**
      * Cancel the user invitation.
      */
-    public function __invoke(Request $request , Invitation $invitation): RedirectResponse
+    public function __invoke(Request $request, Invitation $invitation): RedirectResponse
     {
         $invitation->forceFill(['state' => 'cancelled'])->save();
         FlashMessage::make()->success(
             message: __('Invitation Cancelled Successfully')
         )->closeable()->send();
-        return redirect()->route('invitation.index');
+        return redirect()->route('hire.index');
     }
 }
