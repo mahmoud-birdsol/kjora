4c2702c659dfcc385e555fb0a0fd34a7414d33bd
Peter Emad <peteremad95@gmail.com>
2023-05-03 11:50:10 +0300

    send the time in create invitation as utc

--- resources/js/Pages/Invitation/Create.vue
+++ resources/js/Pages/Invitation/Create.vue
@@ -36,7 +36,7 @@ const createInvitation = () => {
     if (isDisabled.value) return
 
     form.date = dayjs(form.date).add(1).format('YYYY-MM-DD');
-
+    form.time = (new Date(form.time)).toISOString()
     form.post(route('invitation.store'), {
         onStart: () => {
             isDisabled.value = true
@@ -126,7 +126,9 @@ const querySearch = (queryString, cb) => {
                                 <div class="my-6">
                                     <InputLabel>{{ $t('Date') }}</InputLabel>
                                     <div class="px-4">
-                                        <ElDatePicker type="date" :disabled-date="disabledDate" style="background-color: black;" v-model="form.date" @change="disabledHours()" class="w-full" placeholde="DD/MM/YYYY" />
+                                        <ElDatePicker type="date" :disabled-date="disabledDate"
+                                            style="background-color: black;" v-model="form.date" @change="disabledHours()"
+                                            class="w-full" placeholde="DD/MM/YYYY" />
                                     </div>
                                     <InputError class="mt-2" :message="form.errors.date" />
                                 </div>
@@ -134,7 +136,8 @@ const querySearch = (queryString, cb) => {
                                 <div class="my-6">
                                     <InputLabel>{{ $t('Time') }}</InputLabel>
                                     <div class="px-4">
-                                        <ElTimePicker placeholder="Pick the time " format="HH:mm" v-model="form.time" :disabled-hours="disabledHours" />
+                                        <ElTimePicker placeholder="Pick the time " format="HH:mm" v-model="form.time"
+                                            :disabled-hours="disabledHours" />
                                     </div>
                                     <InputError class="mt-2" :message="form.errors.time" />
                                 </div>
@@ -142,7 +145,9 @@ const querySearch = (queryString, cb) => {
                                 <div class="my-6">
                                     <InputLabel>{{ $t('Stadium') }}</InputLabel>
                                     <div class="px-4 ">
-                                        <RichSelectInput :options="stadiums" value-name="id" text-name="name" :image-name="null" v-model="form.stadium_id" bgColor="black" txtColor="white" @selected="changeMapMarker" />
+                                        <RichSelectInput :options="stadiums" value-name="id" text-name="name"
+                                            :image-name="null" v-model="form.stadium_id" bgColor="black" txtColor="white"
+                                            @selected="changeMapMarker" />
                                     </div>
                                     <InputError class="mt-2" :message="form.errors.stadium_id" />
                                 </div>
@@ -177,7 +182,8 @@ const querySearch = (queryString, cb) => {
                             <h2 class="text-xl font-bold uppercase text-primary">{{ $t('Invitation Sent') }}</h2>
                         </div>
                         <p class="">
-                            {{ $t('Your invitation will be sent and you will receive an email updating you on the status of your request') }}.</p>
+                            {{ $t('Your invitation will be sent and you will receive an email updating you on the status of
+                                                        your request') }}.</p>
 
                         <Link :href="route('home')" class="flex w-full min-w-full">
                         <PrimaryButton class="w-full" @click="showSuccessModal = false">{{ $t('Ok') }}</PrimaryButton>
@@ -276,5 +282,4 @@ const querySearch = (queryString, cb) => {
 
 .el-input__inner {
     color: white;
-}
-</style>
+}</style>
