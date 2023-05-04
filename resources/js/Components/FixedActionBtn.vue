94051673960f55e776bcb9f77adfa984da4b64df
aml walaed <amlwalaed@gmail.com>
2023-05-04 15:53:11 +0300

    align filter with chat icon

--- resources/js/Components/FixedActionBtn.vue
+++ resources/js/Components/FixedActionBtn.vue
@@ -4,7 +4,7 @@ defineEmits(["clicked"]);
 <template>
     <!-- @click="$emit('clicked')" -->
     <div
-        class="fixed bottom-0 right-0 z-10 p-4 pb-32 pointer-events-none sm:pb-24"
+        class="fixed bottom-0 z-10 pb-20 pointer-events-none right-[20px] sm:pb-24"
     >
         <button
             class="flex items-center justify-center w-16 h-16 text-center bg-black rounded-full shadow-xl pointer-events-auto"
