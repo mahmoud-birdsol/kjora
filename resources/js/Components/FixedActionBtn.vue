df95f791a96b6c6b8a5df771f615f9edddd2e213
aml walaed <amlwalaed@gmail.com>
2023-05-07 18:16:01 +0300

    FIX

--- resources/js/Components/FixedActionBtn.vue
+++ resources/js/Components/FixedActionBtn.vue
@@ -4,7 +4,7 @@ defineEmits(["clicked"]);
 <template>
     <!-- @click="$emit('clicked')" -->
     <div
-        class="fixed bottom-0 z-10 pb-20 pointer-events-none right-[20px] sm:pb-24"
+        class="fixed bottom-0 z-10 pb-20 pointer-events-none sm:pb-24 BeaconFabButtonFrame"
     >
         <button
             class="flex items-center justify-center w-16 h-16 text-center bg-black rounded-full shadow-xl pointer-events-auto"
