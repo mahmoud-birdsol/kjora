fc06d73f49dad18d4c829e65f9939e5cbc6b0f5e
ahmy1996404 <a.hamodys@yahoo.com>
2023-05-23 14:28:56 +0300

    If the user has two invitations at the same time, if one is accepted then the other should be canceled

--- app/Http/Controllers/AcceptInvitationController.php
+++ app/Http/Controllers/AcceptInvitationController.php
@@ -7,6 +7,7 @@ use App\Models\Invitation;
 use App\Notifications\GameScheduledNotification;
 use App\Notifications\InvitationAcceptedNotification;
 use App\Services\FlashMessage;
+use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
@@ -28,11 +29,22 @@ class AcceptInvitationController extends Controller
             $invitation->invitingPlayer->notify(new InvitationAcceptedNotification($invitation));
             $invitation->invitedPlayer->notify(new GameScheduledNotification($invitation));
 
+            $this->changeStateOfSameDateInvitationsToCancelled($request->user()->invitations , $invitation);
+            $this->changeStateOfSameDateInvitationsToCancelled($request->user()->hires , $invitation);
+
         }else{
             FlashMessage::make()->error(
                 message: __('Sorry you cannot approve cancelled invitation')
             )->closeable()->send();
         }
         return redirect()->route('invitation.index');
+    }
+    private function changeStateOfSameDateInvitationsToCancelled( $invitations , $invitation){
+        $querys = $invitations->where('state',null)->whereBetween('date', [Carbon::parse($invitation->date), Carbon::parse($invitation->date->addHours(2))]);
+
+        foreach ($querys as $query){
+            $query->state =  "cancelled";
+            $query->save();
+        }
     }
 }
