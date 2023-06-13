3bbf8650f0dff2f8e5b5d72cb9adb7455bfdc688
aml walaed <amlwalaed@gmail.com>
2023-06-13 15:48:27 +0300

    fix issue :on left on the menu there should be only one line not two

--- resources/js/Components/ResponsiveNavLink.vue
+++ resources/js/Components/ResponsiveNavLink.vue
@@ -7,21 +7,21 @@ const props = defineProps({
     href: String,
     as: String,
 });
-
+const sharedClasses = 'block w-full py-2 pl-3 pr-4 text-base font-medium text-white uppercase transition rtl:border-r-4 ltr:border-l-4 hover:bg-gray-50 focus:outline-none focus:text-primary hover:text-primary'
 const classes = computed(() => {
     return props.active
-        ? 'block pl-3 pr-4 py-2 rtl:border-r-4 ltr:border-l-4 border-primary text-base font-medium text-white bg-transparent focus:outline-none focus:text-primary focus:bg-transparent focus:border-primary transition uppercase'
-        : 'block pl-3 pr-4 py-2 rtl:border-r-4 ltr:border-l-4 border-transparent text-base font-medium text-white hover:text-white hover:bg-transparent hover:border-primary focus:outline-none focus:text-primary focus:bg-gray-50 focus:border-primary transition uppercase';
+        ? 'border-primary bg-transparent focus:bg-transparent' + '' + sharedClasses
+        : 'border-transparent' + ' ' + sharedClasses;
 });
 </script>
 
 <template>
     <div>
-        <button v-if="as == 'button'" :class="classes" class="w-full ltr:text-left rtl:text-right">
+        <button v-if="as == 'button'" :class="classes">
             <slot />
         </button>
 
-        <Link v-else :href="href" class="flex gap-2 items-center" :class="classes">
+        <Link v-else :href="href" class="flex items-center gap-2" :class="classes">
         <slot />
         </Link>
     </div>
