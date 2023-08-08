9a15eae20432412eb5f0d8c032cfb9f7206bec11
aml walaed <amlwalaed@gmail.com>
2023-08-08 16:27:35 +0300

    make store composition api

--- resources/js/stores/chat.js
+++ resources/js/stores/chat.js
@@ -1,284 +1,307 @@
 import { defineStore } from "pinia";
 import axios from "axios";
+import { computed, ref } from "vue";
 
-export const useChat = defineStore("chat", {
-    state() {
-        return {
-            messages: [],
-            page: 1,
-            lastPage: 0,
-            showLastPageNotice: false,
-            loading: false,
-            container: null,
-            conversation: null,
-            search: "",
-            replyToMessage: null,
-            currentUserId: null,
-            interval: null,
-        };
-    },
+export const useChat = defineStore("chat", () => {
+   const messages = ref([]);
+   const page = ref(1);
+   const lastPage = ref(0);
+   const showLastPageNotice = ref(false);
+   const loading = ref(false);
+   const container = ref(null);
+   const conversation = ref(null);
+   const search = ref("");
+   const replyToMessage = ref(null);
+   const currentUserId = ref(null);
+   const interval = ref(null);
 
-    getters: {
-        /**
-         * Get store messages.
-         *
-         * @param state
-         * @returns {Array}
-         */
-        messageList(state) {
-            return state.messages;
-        },
+   /**
+    * Get store messages.
+    *
+    * @param state
+    * @returns {Array}
+    */
+   const messageList = computed(() => {
+      return messages.value;
+   });
+   /**
+    * Check if the last page is reached.
+    *
+    * @param state
+    * @returns {boolean}
+    */
+   const lastPageReached = computed(() => {
+      return page.value === lastPage.value;
+   });
 
-        /**
-         * Check if the last page is reached.
-         *
-         * @param state
-         * @returns {boolean}
-         */
-        lastPageReached(state) {
-            return state.page === state.lastPage;
-        },
+   /**
+    * Get the loading state of the store.
+    *
+    * @param state
+    * @returns {*}
+    */
+   const isLoading = computed(() => {
+      return loading.value;
+   });
 
-        /**
-         * Get the loading state of the store.
-         *
-         * @param state
-         * @returns {*}
-         */
-        isLoading(state) {
-            return state.loading;
-        },
+   /**
+    * Get the messages seleced for reply.
+    *
+    * @param state
+    * @returns {null|*}
+    */
+   const repliedMessage = computed(() => {
+      return replyToMessage.value;
+   });
 
-        /**
-         * Get the messages seleced for reply.
-         *
-         * @param state
-         * @returns {null|*}
-         */
-        repliedMessage(state) {
-            return state.replyToMessage;
-        },
-    },
-
-    actions: {
-        /**
-         * Initialize the store.
-         *
-         * @param conversation
-         * @param messagesContainer
-         * @private
-         */
-        initialize({ conversation, container, currentUserId }) {
-            this.conversation = conversation;
-            this.container = container;
-            this.currentUserId = currentUserId;
-            this.subscribeToConversation();
-            this.messages = [];
-            this.fetchMessages();
-            //fetch messages every 30 sec for case pusher doesn't work well
-            this.interval = setInterval(this.fetchNewMessages, 30000);
-            // Add event listener for the container
-            // scroll to load more messages.
-            setTimeout(() => {
-                this.registerScrollListener();
-            }, 3000);
-        },
-        /**
-         * Subscribe to the conversation channel.
-         */
-        subscribeToConversation() {
-            Echo.private(`users.chat.${this.conversation.id}`)
-                .listen(".message-sent", (event) => {
-                    if (this.currentUserId !== event.sender_id) {
-                        this.messages.unshift({ ...event, new: true });
-                        this.scrollToMessagesBottom();
-                        axios.post(route("api.message.mark-as-read", event.id));
-                    }
-                })
-                .listen(".message-read", (event) => {
-                    this.setMessageAsRead(event);
-                });
-        },
-        /**
-         * Fetch the conversation messages from api.
-         *
-         * @returns {Promise<*>}
-         */
-        async fetchMessages() {
-            this.loading = true;
-            try {
-                let response = await axios.get(
-                    route(`api.messages.index`, this.conversation),
-                    {
-                        params: {
-                            page: this.page,
-                            search: this.search,
-                        },
-                    }
-                );
-                this.messages = this.messages.concat(response.data.data);
-                this.lastPage = response.data.meta.last_page;
-                this.loading = false;
-
-                //mark messages that not read as read on fetch
-                this.messages.forEach((m) => {
-                    if (
-                        m.read_at == null &&
-                        m.sender_id !== this.currentUserId
-                    ) {
-                        axios.post(route("api.message.mark-as-read", m.id));
-                    }
-                });
-
-                this.page === 1 ? this.scrollToMessagesBottom() : null;
-            } catch (error) {
-                this.loading = false;
-                return error;
+   // actions
+   /**
+    * Initialize the store.
+    *
+    * @param conversation
+    * @param messagesContainer
+    * @private
+    */
+   function initialize({
+      conversation: conversationParam,
+      container: containerParam,
+      currentUserId: currentUserIdParam,
+   }) {
+      conversation.value = conversationParam;
+      container.value = containerParam;
+      currentUserId.value = currentUserIdParam;
+      subscribeToConversation();
+      messages.value = [];
+      fetchMessages();
+      //fetch messages every 30 sec for case pusher doesn't work well
+      interval.value = setInterval(fetchNewMessages, 30000);
+      // Add event listener for the container
+      // scroll to load more messages.
+      setTimeout(() => {
+         registerScrollListener();
+      }, 3000);
+   }
+   /**
+    * Subscribe to the conversation channel.
+    */
+   function subscribeToConversation() {
+      Echo.private(`users.chat.${conversation.value.id}`)
+         .listen(".message-sent", (event) => {
+            if (currentUserId.value !== event.sender_id) {
+               messages.value.unshift({ ...event, new: true });
+               scrollToMessagesBottom();
+               axios.post(route("api.message.mark-as-read", event.id));
             }
-        },
+         })
+         .listen(".message-read", (event) => {
+            setMessageAsRead(event);
+         });
+   }
 
-        /**
-         * Fetch the new conversation  messages from api .
-         *
-         * @returns {Promise<*>}
-         */
-        async fetchNewMessages() {
-            // this.loading = true;
-            try {
-                let response = await axios.get(
-                    route(`api.messages.new`, this.conversation)
-                );
-
-                let newMessages = response.data.data;
-                newMessages.forEach((newM) => {
-                    // check if the pusher didn't work correctly then the message will not be in the messages array
-                    if (!this.messages.some((m) => m.id === newM.id)) {
-                        this.messages.unshift({ ...newM, new: true });
-                        this.scrollToMessagesBottom();
-                    }
-                });
-            } catch (error) {
-                this.loading = false;
-                return error;
+   /**
+    * Fetch the conversation messages from api.
+    *
+    * @returns {Promise<*>}
+    */
+   async function fetchMessages() {
+      loading.value = true;
+      try {
+         let response = await axios.get(
+            route(`api.messages.index`, conversation.value),
+            {
+               params: {
+                  page: page.value,
+                  search: search.value,
+               },
             }
-        },
-        /**
-         * Search conversation messages.
-         *
-         * @returns {Promise<void>}
-         */
-        async searchMessages() {
-            this.page = 1;
-            this.messages = [];
-            await this.fetchMessages();
-        },
-        deleteMessage(id) {
-            let index = this.messages.findIndex((m) => m.id === id);
-            this.messages.splice(index, 1);
-        },
+         );
+         messages.value = messages.value.concat(response.data.data);
+         lastPage.value = response.data.meta.last_page;
+         loading.value = false;
 
-        /**
-         * mark message as read in ui
-         * @param message
-         */
-        setMessageAsRead(message) {
-            let oldMessageIndex = this.messages.findIndex(
-                (m) => m.id === message.id
-            );
-            this.messages[oldMessageIndex].read_at = message.read_at;
-        },
-        /**
-         * Register the container scroll listener.
-         */
-        registerScrollListener() {
-            this.container
-                ? this.container.addEventListener("scroll", this.handleScroll)
-                : null;
-        },
-
-        /**
-         * Un register the container scroll listener.
-         */
-        unRegisterScrollListener() {
-            this.container
-                ? this.container.removeEventListener(
-                      "scroll",
-                      this.handleScroll
-                  )
-                : null;
-        },
-        /**
-         * Unsubscribe from chat channel.
-         */
-        UnsubscribeFromChatChannel() {
-            Echo.leave(`users.chat.${this.conversation.id}`);
-        },
-        /**
-         * clear fetch new message interval.
-         */
-        clearFetchNewMessages() {
-            clearInterval(this.interval);
-        },
-
-        /**
-         * Select a message to reply to
-         *
-         * @param message
-         */
-        setMessageToReplyTo(message) {
-            this.replyToMessage = message;
-        },
-
-        /**
-         * Push a new message.
-         *
-         * @param message
-         */
-        pushNewMessage(message) {
-            this.messages.unshift(message);
-            this.scrollToMessagesBottom();
-            this.replyToMessage = null;
-        },
-
-        /**
-         * SCROLL FUNCTIONALITY.
-         */
-
-        scrollToMessagesBottom() {
-            let vm = this;
-            setTimeout(() => {
-                vm.container.scrollTo({
-                    top: vm.container.scrollHeight,
-                    left: 0,
-                    behavior: "smooth",
-                });
-            }, 200);
-        },
-
-        scrollToMessagesTop() {
-            let vm = this;
-            setTimeout(() => {
-                vm.container.scrollTo({
-                    top: 0,
-                    left: 0,
-                    behavior: "smooth",
-                });
-            }, 200);
-        },
-
-        handleScroll(e) {
-            let element = e.target;
-
-            if (this.lastPageReached) {
-                // Do nothing.
-                this.showLastPageNotice = true;
-                return;
+         //mark messages that not read as read on fetch
+         messages.value.forEach((m) => {
+            if (m.read_at == null && m.sender_id !== currentUserId.value) {
+               axios.post(route("api.message.mark-as-read", m.id));
             }
+         });
 
-            if (element.scrollTop === 0) {
-                this.page++;
-                this.fetchMessages();
+         page.value === 1 ? scrollToMessagesBottom() : null;
+      } catch (error) {
+         loading.value = false;
+         return error;
+      }
+   }
+
+   /**
+    * Fetch the new conversation  messages from api .
+    *
+    * @returns {Promise<*>}
+    */
+   async function fetchNewMessages() {
+      try {
+         let response = await axios.get(
+            route(`api.messages.new`, conversation.value)
+         );
+
+         let newMessages = response.data.data;
+         newMessages.forEach((newM) => {
+            // check if the pusher didn't work correctly then the message will not be in the messages array
+            if (!messages.value.some((m) => m.id === newM.id)) {
+               messages.value.unshift({ ...newM, new: true });
+               scrollToMessagesBottom();
             }
-        },
-    },
+         });
+      } catch (error) {
+         loading.value = false;
+         return error;
+      }
+   }
+
+   /**
+    * Search conversation messages.
+    *
+    * @returns {Promise<void>}
+    */
+   async function searchMessages() {
+      page.value = 1;
+      messages.value = [];
+      await fetchMessages();
+   }
+   function deleteMessage(id) {
+      let index = messages.value.findIndex((m) => m.id === id);
+      messages.value.splice(index, 1);
+   }
+   /**
+    * mark message as read in ui
+    * @param msg
+    */
+   function setMessageAsRead(msg) {
+      let oldMessageIndex = messages.value.findIndex((m) => m.id === msg.id);
+      messages.value[oldMessageIndex].read_at = msg.read_at;
+   }
+
+   /**
+    * Register the container scroll listener.
+    */
+   function registerScrollListener() {
+      container.value
+         ? container.value.addEventListener("scroll", handleScroll)
+         : null;
+   }
+
+   /**
+    * Un register the container scroll listener.
+    */
+   function unRegisterScrollListener() {
+      container.value
+         ? container.value.removeEventListener("scroll", handleScroll)
+         : null;
+   }
+
+   /**
+    * Unsubscribe from chat channel.
+    */
+   function UnsubscribeFromChatChannel() {
+      Echo.leave(`users.chat.${conversation.value?.id}`);
+   }
+   /**
+    * clear fetch new message interval.
+    */
+   function clearFetchNewMessages() {
+      clearInterval(interval.value);
+   }
+
+   /**
+    * Select a message to reply to
+    *
+    * @param message
+    */
+   function setMessageToReplyTo(message) {
+      replyToMessage.value = message;
+   }
+   /**
+    * Push a new message.
+    *
+    * @param message
+    */
+   function pushNewMessage(msg) {
+      messages.value?.unshift(msg);
+      scrollToMessagesBottom();
+      replyToMessage.value = null;
+   }
+   /**
+    * SCROLL FUNCTIONALITY.
+    */
+
+   function scrollToMessagesBottom() {
+      setTimeout(() => {
+         container.value.scrollTo({
+            top: container.value.scrollHeight,
+            left: 0,
+            behavior: "smooth",
+         });
+      }, 200);
+   }
+   function scrollToMessagesTop() {
+      setTimeout(() => {
+         container.value.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+         });
+      }, 200);
+   }
+   function handleScroll(e) {
+      let element = e.target;
+
+      if (lastPageReached.value) {
+         // Do nothing.
+         showLastPageNotice.value = true;
+         return;
+      }
+
+      if (element.scrollTop === 0) {
+         page.value += 1;
+         fetchMessages();
+      }
+   }
+   return {
+      // state
+      messages,
+      page,
+      lastPage,
+      showLastPageNotice,
+      loading,
+      container,
+      conversation,
+      search,
+      replyToMessage,
+      currentUserId,
+      interval,
+
+      //   getter
+      lastPageReached,
+      messageList,
+      isLoading,
+      repliedMessage,
+
+      //   actions
+      initialize,
+      subscribeToConversation,
+      fetchMessages,
+      fetchNewMessages,
+      searchMessages,
+      deleteMessage,
+      setMessageAsRead,
+      registerScrollListener,
+      unRegisterScrollListener,
+      UnsubscribeFromChatChannel,
+      clearFetchNewMessages,
+      setMessageToReplyTo,
+      pushNewMessage,
+      scrollToMessagesBottom,
+      scrollToMessagesTop,
+      handleScroll,
+   };
 });
