bcbe97fbe274b1de6300240aee379004a64c7631
aml walaed <109831373+amlWalaed@users.noreply.github.com>
2023-08-13 13:09:54 +0300

    Input upload (#185)
    * file input
    
    * move uploadGalleryFile
    
    * FIX

--- resources/js/Components/UploadImageField.vue
+++ resources/js/Components/UploadImageField.vue
@@ -3,246 +3,217 @@ import Crop from "@/Components/Crop.vue";
 import InputError from "@/Components/Forms/InputError.vue";
 import Modal from "@/Components/Modal.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
-import { PlusCircleIcon, XMarkIcon } from "@heroicons/vue/24/outline";
+import { XMarkIcon } from "@heroicons/vue/24/outline";
 import { useForm } from "@inertiajs/vue3";
 import { onMounted, ref, watch } from "vue";
 import Title from "./Title.vue";
+import InputUpload from "@/Components/Forms/InputUpload.vue";
 
 const props = defineProps({
-    modelValue: {
-        required: false,
-        default: null,
-    },
-    shouldUpload: {
-        require: false,
-        type: Boolean,
-        default: false,
-    },
-    currentImageUrl: {
-        required: false,
-        type: String,
-        default: null,
-    },
-    modelName: {
-        required: false,
-        type: String,
-    },
-    modelId: {
-        required: false,
-        type: Number,
-    },
-    collectionName: {
-        required: false,
-        type: String,
-    },
-    show: {
-        type: Boolean,
-        default: false,
-    },
-    maxWidth: {
-        type: String,
-        default: "2xl",
-    },
-    closeable: {
-        type: Boolean,
-        default: true,
-    },
-    position: {
-        required: false,
-        type: String,
-        default: "center",
-    },
+   modelValue: {
+      required: false,
+      default: null,
+   },
+   shouldUpload: {
+      require: false,
+      type: Boolean,
+      default: false,
+   },
+   currentImageUrl: {
+      required: false,
+      type: String,
+      default: null,
+   },
+   modelName: {
+      required: false,
+      type: String,
+   },
+   modelId: {
+      required: false,
+      type: Number,
+   },
+   collectionName: {
+      required: false,
+      type: String,
+   },
+   show: {
+      type: Boolean,
+      default: false,
+   },
+   maxWidth: {
+      type: String,
+      default: "2xl",
+   },
+   closeable: {
+      type: Boolean,
+      default: true,
+   },
+   position: {
+      required: false,
+      type: String,
+      default: "center",
+   },
 });
 
 const emit = defineEmits(["close", "update:modelValue", "selected"]);
 
+const files = ref([]);
 const showPreview = ref(false);
 const previewImageUrl = ref(null);
 const photoInput = ref(null);
 const fileData = ref(null);
-const cropLoading = ref(false);
+const isCropLoading = ref(false);
 const form = useForm({
-    model_name: props.modelName,
-    model_id: props.modelId,
-    collection_name: props.collectionName,
-    image: null,
+   model_name: props.modelName,
+   model_id: props.modelId,
+   collection_name: props.collectionName,
+   image: null,
 });
 const num = ref(Math.floor(Math.random() * 100));
 const cropFile = ref({});
 const openCropModal = ref(false);
 
 onMounted(() => {
-    if (props.currentImageUrl) {
-        showPreview.value = true;
-        previewImageUrl.value = props.currentImageUrl;
-    }
+   if (props.currentImageUrl) {
+      showPreview.value = true;
+      previewImageUrl.value = props.currentImageUrl;
+   }
 });
 
 watch(
-    () => props.currentImageUrl,
-    () => {
-        if (props.currentImageUrl) {
-            showPreview.value = true;
-            previewImageUrl.value = props.currentImageUrl;
-        }
-    }
+   () => props.currentImageUrl,
+   () => {
+      if (props.currentImageUrl) {
+         showPreview.value = true;
+         previewImageUrl.value = props.currentImageUrl;
+      }
+   }
 );
 
-const selectNewPhoto = () => {
-    photoInput.value.value = "";
-    photoInput.value.click();
-};
-
-const updatePhotoPreview = () => {
-    if (!photoInput.value.files.length) return;
-
-    fileData.value = photoInput.value.files[0];
-
-    cropLoading.value = true;
-    const reader = new FileReader();
-
-    reader.onload = (e) => {
-        previewImageUrl.value = e.target.result;
-        showPreview.value = true;
-    };
-
-    reader.readAsDataURL(fileData.value);
-    reader.onloadend = () => {
-        showCropModal(previewImageUrl);
-    };
-};
-
 const removePhoto = () => {
-    showPreview.value = false;
-    previewImageUrl.value = null;
-    cropFile.value = {};
-    openCropModal.value = false;
+   showPreview.value = false;
+   previewImageUrl.value = null;
+   cropFile.value = {};
+   openCropModal.value = false;
 };
 
 const upload = () => {
-    if (!props.shouldUpload) {
-        emit("update:modelValue", fileData.value);
-        emit("selected", previewImageUrl.value);
-        close();
-        return;
-    }
+   if (!props.shouldUpload) {
+      emit("update:modelValue", fileData.value);
+      emit("selected", previewImageUrl.value);
+      close();
+      return;
+   }
 
-    if (fileData.value) {
-        form.image = fileData.value;
-    }
+   if (fileData.value) {
+      form.image = fileData.value;
+   }
 
-    form.post(route("upload"), {
-        preserveState: true,
-        preserveScroll: true,
-        onFinish: (response) => {
-            close();
-        },
-    });
+   form.post(route("upload"), {
+      preserveState: true,
+      preserveScroll: true,
+      onFinish: (_) => {
+         close();
+      },
+   });
 };
-function changeFiles(file, url, id) {
-    fileData.value = file;
-    previewImageUrl.value = url;
-    cropFile.value = {};
+function changeFiles(file, url, _id) {
+   fileData.value = file;
+   previewImageUrl.value = url;
+   cropFile.value = {};
 }
 let showCropModal = (url) => {
-    cropFile.value = {
-        name: fileData.value.name,
-        url,
-    };
+   cropFile.value = {
+      name: fileData.value.name,
+      url,
+   };
 
-    openCropModal.value = true;
-    cropLoading.value = false;
+   openCropModal.value = true;
+   isCropLoading.value = false;
 };
 function close() {
-    fileData.value = null;
-    num.value += 1;
-    emit("close");
+   fileData.value = null;
+   num.value += 1;
+   emit("close");
 }
+
+const loadFiles = (newFiles, newFileData) => {
+   fileData.value = newFileData.file;
+   previewImageUrl.value = newFileData.previewUrl;
+   showPreview.value = true;
+   isCropLoading.value = true;
+   showCropModal(newFileData.url);
+};
 </script>
 
 <template>
-    <Modal
-        :show="show"
-        :max-width="maxWidth"
-        :closeable="closeable"
-        :position="position"
-        @close="close"
-        :key="num"
-    >
-        <div class="flex flex-col min-h-[500px] justify-between p-6 pt-0">
-            <Title>{{ $t("upload") }}</Title>
-            <div
-                class="flex items-center justify-center py-8 sm:px-20"
-                v-loading="form.processing"
-            >
-                <div class="max-w-[300px] w-full">
-                    <input
-                        ref="photoInput"
-                        type="file"
-                        class="hidden"
-                        @input="updatePhotoPreview"
-                    />
-
-                    <div class="flex items-center justify-center mb-6">
-                        <button
-                            type="button"
-                            class="inline-flex items-center p-4 text-white bg-black border border-transparent rounded-full shadow-sm hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2"
-                            @click.prevent="selectNewPhoto"
-                        >
-                            <PlusCircleIcon class="w-5 h-5" />
-                        </button>
-                    </div>
-
-                    <button
-                        v-show="showPreview && !openCropModal"
-                        @click.prevent="removePhoto"
-                        class="relative w-full rounded"
-                        v-loading="cropLoading"
-                    >
-                        <img
-                            :src="previewImageUrl"
-                            alt=""
-                            class="w-full h-auto rounded-lg"
-                            id="img"
-                        />
-                        <div
-                            class="absolute inset-0 w-full h-full bg-transparent hover:bg-white hover:bg-opacity-50"
-                        >
-                            <div
-                                class="flex flex-col items-center justify-center h-full opacity-0 hover:opacity-100"
-                            >
-                                <XMarkIcon class="w-8 h-8 text-white" />
-                            </div>
-                        </div>
-                    </button>
-                </div>
+   <Modal
+      :show="show"
+      :max-width="maxWidth"
+      :closeable="closeable"
+      :position="position"
+      @close="close"
+      :key="num"
+   >
+      <div class="flex flex-col min-h-[500px] justify-between p-6 pt-0">
+         <Title>{{ $t("upload") }}</Title>
+         <div
+            class="flex items-center justify-center py-8 sm:px-20"
+            v-loading="form.processing"
+         >
+            <div class="max-w-[300px] w-full">
+               <InputUpload
+                  :accept="['image', 'video']"
+                  :isDisabled="isDisabled"
+                  @onFinish="loadFiles"
+               />
+               <button
+                  v-show="showPreview && !openCropModal"
+                  @click.prevent="removePhoto"
+                  class="relative w-full rounded"
+                  v-loading="isCropLoading"
+               >
+                  <img
+                     :src="previewImageUrl"
+                     alt=""
+                     class="w-full h-auto rounded-lg"
+                     id="img"
+                  />
+                  <div
+                     class="absolute inset-0 w-full h-full bg-transparent hover:bg-white hover:bg-opacity-50"
+                  >
+                     <div
+                        class="flex flex-col items-center justify-center h-full opacity-0 hover:opacity-100"
+                     >
+                        <XMarkIcon class="w-8 h-8 text-white" />
+                     </div>
+                  </div>
+               </button>
             </div>
-            <div>
-                <Crop
-                    :img="cropFile"
-                    @crop="changeFiles"
-                    v-model:open="openCropModal"
-                    @update:open="() => (openCropModal = false)"
-                    :presetMode="{
-                        mode: 'round',
-                        width: 250,
-                        height: 250,
-                    }"
-                    :addOption="{
-                        viewMode: 1,
-                        dragMode: 'move',
-                        aspectRatio: 1,
-                        cropBoxResizable: false,
-                    }"
-                />
-                <PrimaryButton
-                    @click.prevent="upload"
-                    :disabled="form.processing"
-                >
-                    {{ $t("upload") }}
-                </PrimaryButton>
-                <InputError class="mt-2" :message="form.errors.image" />
-            </div>
-        </div>
-    </Modal>
+         </div>
+         <div>
+            <Crop
+               :img="cropFile"
+               @crop="changeFiles"
+               v-model:open="openCropModal"
+               @update:open="() => (openCropModal = false)"
+               :presetMode="{
+                  mode: 'round',
+                  width: 250,
+                  height: 250,
+               }"
+               :addOption="{
+                  viewMode: 1,
+                  dragMode: 'move',
+                  aspectRatio: 1,
+                  cropBoxResizable: false,
+               }"
+            />
+            <PrimaryButton @click.prevent="upload" :disabled="form.processing">
+               {{ $t("upload") }}
+            </PrimaryButton>
+            <InputError class="mt-2" :message="form.errors.image" />
+         </div>
+      </div>
+   </Modal>
 </template>
