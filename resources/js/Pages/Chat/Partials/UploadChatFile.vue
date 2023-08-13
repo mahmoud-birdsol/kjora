66667fcaf5816eda700b8439c5f566479fd26a4c
aml walaed <109831373+amlWalaed@users.noreply.github.com>
2023-08-13 13:31:02 +0300

    Revert "Input upload (#185)"
    This reverts commit bcbe97fbe274b1de6300240aee379004a64c7631.

--- resources/js/Pages/Chat/Partials/UploadChatFile.vue
+++ resources/js/Pages/Chat/Partials/UploadChatFile.vue
@@ -1,15 +1,13 @@
 <script setup>
 import Crop from "@/Components/Crop.vue";
 import FadeInTransition from "@/Components/FadeInTransition.vue";
-import InputUpload from "@/Components/Forms/InputUpload.vue";
 import Modal from "@/Components/Modal.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
-import useGetAllowedUploadFiles from "@/Composables/useGetAllowedUploadFiles.js";
-import { XMarkIcon } from "@heroicons/vue/24/outline";
-import { PlayIcon } from "@heroicons/vue/24/solid";
+import { PlusCircleIcon, XMarkIcon } from "@heroicons/vue/24/outline";
 import { usePage } from "@inertiajs/vue3";
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import VueEasyLightbox from "vue-easy-lightbox";
+
 const props = defineProps({
    modelValue: {
       required: false,
@@ -60,6 +58,7 @@ const emit = defineEmits(["close", "update:modelValue", "reload", "upload"]);
 
 const showPreview = ref(false);
 const filesData = ref([]);
+const photoInput = ref(null);
 const isLoading = ref(false);
 const isDisabled = ref(false);
 const num = ref(0);
@@ -67,12 +66,11 @@ const visibleRef = ref(false);
 const imgsRef = ref(null);
 const cropFile = ref([]);
 const openModal = ref(false);
-const maximumUploadNumberOfFiles = computed(
-   () => usePage().props.maximumUploadNumberOfFiles
+const maximumUploadNumberOfFiles = ref(
+   usePage().props.maximumUploadNumberOfFiles
 );
 
-const showLightBox = (url, type) => {
-   if (type === "pdf" || type === "word") return;
+const showLightBox = (url) => {
    imgsRef.value = url;
    visibleRef.value = true;
 };
@@ -80,13 +78,43 @@ function hideLightBox() {
    visibleRef.value = false;
 }
 
-const loadFiles = (_newFiles, newFilesData) => {
-   newFilesData = useGetAllowedUploadFiles(filesData.value, newFilesData);
-   if (newFilesData.length <= 0) return;
-
-   filesData.value = [...filesData.value, ...newFilesData];
-   showPreview.value = true;
+const selectNewPhoto = () => {
+   photoInput.value.value = null;
+   photoInput.value.click();
 };
+
+const updatePhotoPreview = () => {
+   if (!photoInput.value.files.length) {
+      return;
+   }
+   let newFiles = Array.from(photoInput.value.files).map((file) => {
+      return { file: file, id: _.uniqueId("f") };
+   });
+   newFiles = checkAvailableSize(newFiles);
+   newFiles?.forEach(({ file, id }) => {
+      const url = URL.createObjectURL(file);
+
+      filesData.value.push({
+         file: file,
+         url: url,
+         name: file.name,
+         type: file.type,
+         id: id,
+      });
+      showPreview.value = true;
+   });
+};
+
+function checkAvailableSize(newFiles) {
+   let totalFilesNum = newFiles.length + filesData.value.length;
+   if (totalFilesNum > maximumUploadNumberOfFiles.value) {
+      let availableSize =
+         maximumUploadNumberOfFiles.value - filesData.value.length;
+      if (availableSize <= 0) return;
+      if (availableSize) return (newFiles = newFiles.slice(0, availableSize));
+   }
+   return newFiles;
+}
 
 const removePhoto = (i) => {
    if ((cropFile.value.id, filesData.value[i].id)) {
@@ -123,7 +151,6 @@ let showCropModal = (file) => {
 function changeFiles(file, url, id) {
    let fileUrlIndex = filesData.value.findIndex((f) => f.id === id);
    filesData.value[fileUrlIndex].url = url;
-   filesData.value[fileUrlIndex].previewUrl = url;
    filesData.value[fileUrlIndex].file = file;
    cropFile.value = [];
 }
@@ -148,11 +175,24 @@ function changeFiles(file, url, id) {
          <div class="flex flex-col items-center h-full gap-2 py-8">
             <!-- input -->
             <div class="w-full max-w-[300px] sm:px-20">
-               <InputUpload
+               <input
+                  ref="photoInput"
+                  type="file"
                   multiple
-                  accept="image,video,pdf,word"
-                  @onFinish="loadFiles"
+                  accept="image/*,video/*,.pdf,.doc,.docx"
+                  class="hidden"
+                  @change="updatePhotoPreview"
                />
+               <div class="flex items-center justify-center mb-6">
+                  <button
+                     type="button"
+                     :disabled="isDisabled"
+                     class="inline-flex items-center p-4 text-white bg-black border border-transparent rounded-full shadow-sm enabled: enabled:hover:bg-black enabled:focus:outline-none enabled:focus:ring-2 enabled:focus:ring-black enabled:focus:ring-offset-2 disabled:bg-stone-500"
+                     @click.prevent="selectNewPhoto"
+                  >
+                     <PlusCircleIcon class="w-5 h-5" />
+                  </button>
+               </div>
             </div>
             <!-- preview -->
             <div
@@ -162,36 +202,34 @@ function changeFiles(file, url, id) {
             >
                <div class="relative grid grid-cols-3 gap-2">
                   <template v-for="(file, index) in filesData" :key="index">
-                     <div class="relative">
-                        <template v-if="file.isVideo">
-                           <video
-                              :src="file.previewUrl"
-                              :alt="file.name"
-                              class="object-cover w-full h-full rounded-lg aspect-square"
-                           />
-                           <div
-                              class="absolute inset-0 flex items-center justify-center gap-2 text-xs text-gray-100"
-                           >
-                              <PlayIcon
-                                 class="h-10 filter-[drop-shadow(1px_1px_1px_rgb(0_0_0/.4)]"
-                              />
-                           </div>
-                        </template>
-                        <template v-else>
-                           <img
-                              :src="file.previewUrl"
-                              :alt="file.name"
-                              class="object-contain w-full h-full rounded-lg aspect-square"
-                              @click.stop="showLightBox(file.url, file.type)"
-                           />
-                           <p
-                              v-if="file.isPdf || file.isWord"
-                              class="text-xs text-center text-gray-400 truncate"
-                           >
-                              {{ file.name }}
-                           </p>
-                        </template>
-
+                     <div
+                        v-if="
+                           file.url.startsWith('data:image') ||
+                           file.type.startsWith('image') ||
+                           file.url.startsWith('data:video') ||
+                           file.type.startsWith('video')
+                        "
+                        class="relative"
+                     >
+                        <img
+                           v-if="
+                              file.url.startsWith('data:image') ||
+                              file.type.startsWith('image')
+                           "
+                           :src="file.url"
+                           alt=""
+                           class="object-contain w-full h-full rounded-lg aspect-square"
+                           @click.stop="showLightBox(file.url)"
+                        />
+                        <video
+                           v-if="
+                              file.url.startsWith('data:video') ||
+                              file.type.startsWith('video')
+                           "
+                           :src="file.url"
+                           alt=""
+                           class="object-cover w-full h-full rounded-lg aspect-square"
+                        />
                         <button
                            @click.prevent.stop="removePhoto(index)"
                            class="absolute top-0 right-0 bg-white rounded-bl-xl bg-opacity-90"
@@ -205,7 +243,10 @@ function changeFiles(file, url, id) {
                         <button
                            class="absolute top-0 left-0 bg-white rounded-br-xl bg-opacity-90"
                            @click.stop="showCropModal(file)"
-                           v-if="file.type === 'image'"
+                           v-if="
+                              file.url.startsWith('data:image') ||
+                              file.type.startsWith('image')
+                           "
                         >
                            <div
                               class="flex flex-col items-start justify-center h-full p-1 opacity-100"
@@ -214,6 +255,50 @@ function changeFiles(file, url, id) {
                                  icon="crop-simple"
                                  class="text-xs text-black/70"
                               />
+                           </div>
+                        </button>
+                     </div>
+                     <div
+                        v-else-if="
+                           file.url.startsWith('data:application/pdf') ||
+                           file.type.startsWith('application/pdf')
+                        "
+                        class="relative flex flex-col gap-4"
+                     >
+                        <img class="mx-auto h-36" src="/images/pdf.png" />
+                        <p class="text-xs text-center text-gray-400 truncate">
+                           {{ file.name }}
+                        </p>
+                        <button
+                           @click.prevent="removePhoto(index)"
+                           class="absolute top-0 right-0 bg-white rounded-bl-xl bg-opacity-90"
+                        >
+                           <div
+                              class="flex flex-col items-start justify-center h-full p-1 opacity-100"
+                           >
+                              <XMarkIcon class="w-5 h-5 text-stone-800" />
+                           </div>
+                        </button>
+                     </div>
+                     <div
+                        v-else-if="
+                           file.type.endsWith('.document') ||
+                           file.type.startsWith('application/msword')
+                        "
+                        class="relative flex flex-col gap-4"
+                     >
+                        <img class="mx-auto h-36" src="/images/doc.png" />
+                        <p class="text-xs text-center text-gray-400 truncate">
+                           {{ file.name }}
+                        </p>
+                        <button
+                           @click.prevent="removePhoto(index)"
+                           class="absolute top-0 right-0 bg-white rounded-bl-xl bg-opacity-90"
+                        >
+                           <div
+                              class="flex flex-col items-start justify-center h-full p-1 opacity-100"
+                           >
+                              <XMarkIcon class="w-5 h-5 text-stone-800" />
                            </div>
                         </button>
                      </div>
