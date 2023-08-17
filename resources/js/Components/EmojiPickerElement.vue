fbda8dcbf3530e660c76dd16e63ce6c21c587f63
aml walaed <amlwalaed@gmail.com>
2023-08-17 10:27:02 +0300

    enable emoji picker

--- resources/js/Components/EmojiPickerElement.vue
+++ resources/js/Components/EmojiPickerElement.vue
@@ -1,5 +1,5 @@
 <template>
-   <!-- <EmojiPicker @select="onSelectEmoji" /> -->
+   <EmojiPicker @select="onSelectEmoji" />
 </template>
 
 <script setup>
@@ -9,24 +9,3 @@ const onSelectEmoji = (e) => {
    emits("selectedEmoji", e.i);
 };
 </script>
-
-<!--  Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-/* h3 {
-   margin: 40px 0 0;
-}
-
-ul {
-   list-style-type: none;
-   padding: 0;
-}
-
-li {
-   display: inline-block;
-   margin: 0 10px;
-}
-
-a {
-   color: #42b983;
-} */
-</style>
