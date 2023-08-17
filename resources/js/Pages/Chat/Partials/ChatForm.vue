7963e03135f8f762eb2df19cdffaf2151253ec99
aml walaed <amlwalaed@gmail.com>
2023-08-17 15:24:39 +0300

    fix loading hover to cover reply message also

--- resources/js/Pages/Chat/Partials/ChatForm.vue
+++ resources/js/Pages/Chat/Partials/ChatForm.vue
@@ -47,122 +47,128 @@ function onSelectEmoji(emoji) {
 <template>
    <div class="grid p-6 bg-white gap-y-4 rounded-2xl">
       <!--The gray section to display attachment or a message I am replying to. -->
-      <Transition
-         enter-from-class="opacity-0"
-         enter-to-class="opacity-100"
-         enter-active-class="transition-all duration-150 ease-out"
-         leave-active-class="transition-all duration-150 ease-in"
-         leave-from-class="opacity-100"
-         leave-to-class="opacity-0"
-      >
-         <div
-            v-if="chat.repliedMessage"
-            class="relative flex flex-row items-center justify-between w-full px-2 py-2 text-sm bg-gray-100 group rounded-xl"
+      <div v-loading="chat.isSendingMsg">
+         <Transition
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+            enter-active-class="transition-all duration-150 ease-out"
+            leave-active-class="transition-all duration-150 ease-in"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
          >
             <div
                v-if="chat.repliedMessage"
-               class="flex items-center justify-start space-x-4"
-            >
-               <Avatar
-                  :id="chat.repliedMessage.message_sender.id"
-                  :image-url="chat.repliedMessage.message_sender.avatar_url"
-                  :username="chat.repliedMessage.message_sender.name"
-                  :border="true"
-                  border-color="primary"
-                  size="sm"
-               />
-               <div class="font-bold capitalize text-primary pis-3">
-                  <div>
-                     {{
-                        chat.repliedMessage.sender_id ===
-                        userStore.currentUser.id
-                           ? userStore.currentUser.name
-                           : player.name
-                     }}
-                  </div>
-
-                  <Link
-                     class="text-xs font-normal text-gray-600"
-                     :href="
-                        route(
-                           'player.profile',
-                           chat.repliedMessage.message_sender.id
-                        )
-                     "
-                     >@{{ chat.repliedMessage.message_sender.username }}</Link
-                  >
-               </div>
-               <div class="max-w-[70ch] truncate">
-                  {{ chat.repliedMessage.body }}
-               </div>
-            </div>
-            <div>
-               <div class="text-black max-w-[15rem]">
-                  <div v-if="chat.repliedMessage.attachments.length">
-                     <MediaPreview
-                        :fileType="
-                           useGetFileType(
-                              chat.repliedMessage.attachments[0].mime_type,
-                              chat.repliedMessage.attachments[0].original_url
-                           ).type
-                        "
-                        :filePreview="
-                           chat.repliedMessage.attachments[0].original_url
-                        "
-                        :fileName="chat.repliedMessage.attachments[0].file_name"
-                     />
-                  </div>
-               </div>
-            </div>
-            <button
-               @click.prevent="chat.setMessageToReplyTo(null)"
-               class="absolute top-0 left-0 bg-white bg-opacity-90 rounded-br-xl"
+               class="relative flex flex-row items-center justify-between w-full px-2 py-2 text-sm bg-gray-100 group rounded-xl"
             >
                <div
-                  class="flex flex-col items-start justify-center h-full p-1 opacity-100"
+                  v-if="chat.repliedMessage"
+                  class="flex items-center justify-start space-x-4"
                >
-                  <XMarkIcon class="w-5 h-5 text-stone-800" />
-               </div>
-            </button>
-         </div>
-      </Transition>
+                  <Avatar
+                     :id="chat.repliedMessage.message_sender.id"
+                     :image-url="chat.repliedMessage.message_sender.avatar_url"
+                     :username="chat.repliedMessage.message_sender.name"
+                     :border="true"
+                     border-color="primary"
+                     size="sm"
+                  />
+                  <div class="font-bold capitalize text-primary pis-3">
+                     <div>
+                        {{
+                           chat.repliedMessage.sender_id ===
+                           userStore.currentUser.id
+                              ? userStore.currentUser.name
+                              : player.name
+                        }}
+                     </div>
 
-      <transition
-         enter-from-class="opacity-0"
-         enter-to-class="opacity-100"
-         enter-active-class="transition-all duration-150 ease-out"
-         leave-active-class="transition-all duration-150 ease-in"
-         leave-from-class="opacity-100"
-         leave-to-class="opacity-0"
-      >
-         <div v-loading="chat.isSendingMsg">
-            <div
-               v-if="chat.filesData"
-               class="grid grid-cols-4 gap-2 ml-auto overflow-hidden overflow-y-auto max-h-32 hideScrollBar place-items-center"
-            >
-               <!-- {{ form.attachments }} -->
-               <template v-for="(file, index) in chat.filesData">
-                  <div class="relative w-full">
-                     <MediaPreview
-                        :fileType="file.type"
-                        :filePreview="file.previewUrl"
-                        :fileName="file.name"
-                     />
-                     <button
-                        @click.prevent="removePhoto(index)"
-                        class="absolute top-0 right-0 bg-white bg-opacity-90 rounded-bl-xl"
+                     <Link
+                        class="text-xs font-normal text-gray-600"
+                        :href="
+                           route(
+                              'player.profile',
+                              chat.repliedMessage.message_sender.id
+                           )
+                        "
+                        >@{{
+                           chat.repliedMessage.message_sender.username
+                        }}</Link
                      >
-                        <div
-                           class="flex flex-col items-start justify-center h-full p-1 opacity-100"
-                        >
-                           <XMarkIcon class="w-5 h-5 text-stone-800" />
-                        </div>
-                     </button>
                   </div>
-               </template>
+                  <div class="max-w-[70ch] truncate">
+                     {{ chat.repliedMessage.body }}
+                  </div>
+               </div>
+               <div>
+                  <div class="text-black max-w-[15rem]">
+                     <div v-if="chat.repliedMessage.attachments.length">
+                        <MediaPreview
+                           :fileType="
+                              useGetFileType(
+                                 chat.repliedMessage.attachments[0].mime_type,
+                                 chat.repliedMessage.attachments[0].original_url
+                              ).type
+                           "
+                           :filePreview="
+                              chat.repliedMessage.attachments[0].original_url
+                           "
+                           :fileName="
+                              chat.repliedMessage.attachments[0].file_name
+                           "
+                        />
+                     </div>
+                  </div>
+               </div>
+               <button
+                  @click.prevent="chat.setMessageToReplyTo(null)"
+                  class="absolute top-0 left-0 bg-white bg-opacity-90 rounded-br-xl"
+               >
+                  <div
+                     class="flex flex-col items-start justify-center h-full p-1 opacity-100"
+                  >
+                     <XMarkIcon class="w-5 h-5 text-stone-800" />
+                  </div>
+               </button>
             </div>
-         </div>
-      </transition>
+         </Transition>
+
+         <transition
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+            enter-active-class="transition-all duration-150 ease-out"
+            leave-active-class="transition-all duration-150 ease-in"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+         >
+            <div class="mt-4">
+               <div
+                  v-if="chat.filesData"
+                  class="grid grid-cols-4 gap-2 ml-auto overflow-hidden overflow-y-auto max-h-32 hideScrollBar place-items-center"
+               >
+                  <!-- {{ form.attachments }} -->
+                  <template v-for="(file, index) in chat.filesData">
+                     <div class="relative w-full">
+                        <MediaPreview
+                           :fileType="file.type"
+                           :filePreview="file.previewUrl"
+                           :fileName="file.name"
+                        />
+                        <button
+                           @click.prevent="removePhoto(index)"
+                           class="absolute top-0 right-0 bg-white bg-opacity-90 rounded-bl-xl"
+                        >
+                           <div
+                              class="flex flex-col items-start justify-center h-full p-1 opacity-100"
+                           >
+                              <XMarkIcon class="w-5 h-5 text-stone-800" />
+                           </div>
+                        </button>
+                     </div>
+                  </template>
+               </div>
+            </div>
+         </transition>
+      </div>
 
       <div class="flex flex-row items-center w-full gap-x-3">
          <OnClickOutside
