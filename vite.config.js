1a6a099f6045125cbee0e8875ef87ec130809cd7
amlWalaed <amlwalaed@gmail.com>
2024-10-10 16:02:26 +0300

    [fix] add auto import

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import i18n from 'laravel-vue-i18n/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import i18n from "laravel-vue-i18n/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
+            input: "resources/js/app.js",
+            ssr: "resources/js/ssr.js",
             refresh: true,
         }),
         vue({
@@ -23,18 +23,41 @@ export default defineConfig({
         }),
         i18n(),
         AutoImport({
+            dts: true,
+            dirs: ["./resources/js/Composables", "./resources/js/Utils"],
             resolvers: [ElementPlusResolver()],
+            imports: [
+                "vue",
+                {
+                    "@inertiajs/vue3": ["useForm", "usePage", "router"],
+                },
+            ],
         }),
         Components({
-            resolvers: [ElementPlusResolver()],
+            dts: "auto-components.d.ts",
+            dirs: ["./resources/js/Components", "./resources/js/Layouts"],
+            resolvers: [
+                ElementPlusResolver(),
+                {
+                    resolve: (componentName) => {
+                        if (["Link", "Head"].includes(componentName)) {
+                            return {
+                                name: componentName,
+                                from: "@inertiajs/vue3",
+                            };
+                        }
+                    },
+                    type: "component",
+                },
+            ],
         }),
     ],
     ssr: {
-        noExternal: ['@inertiajs/server'],
+        noExternal: ["@inertiajs/server"],
     },
     resolve: {
         alias: {
-            '@': '/resources/js',
+            "@": "/resources/js",
         },
     },
 });
