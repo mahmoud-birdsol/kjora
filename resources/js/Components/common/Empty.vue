f441e2ee01dd80747450aa8b93422e49ff1424ec
amlWalaed <amlwalaed@gmail.com>
2024-10-17 13:00:31 +0300

    [component] add Empty Component

---
+++ resources/js/Components/common/Empty.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const props = withDefaults(
+	defineProps<{
+		message?: string
+		description?: string
+
+		ui?: {
+			message?: string
+			description?: string
+			image?: string
+		}
+	}>(),
+	{}
+)
+</script>
+
+<template>
+	<div class="flex flex-col items-center gap-1 text-center">
+		<slot name="content">
+			<slot name="image">
+				<img
+					:class="cn('w-28 object-contain text-secondary-200', ui?.image)"
+					src="/images/no-data.webp" />
+			</slot>
+			<slot name="message">
+				<span
+					class="text-xl font-medium text-gray-500 first-letter:capitalize"
+					:class="cn('', ui?.message)"
+					v-text="props.message ?? $t('no-data')" />
+			</slot>
+			<slot name="description">
+				<span
+					class="text-xs text-gray-400 first-letter:capitalize"
+					:class="cn('', ui?.description)"
+					v-text="
+						description ?? $t('there-is-no-data-to-show-you-right-now')
+					" />
+			</slot>
+			<slot v-if="$slots.default" />
+		</slot>
+	</div>
+</template>
+
+<style></style>
--- vite.config.js
+++ vite.config.js
@@ -1,63 +1,64 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import i18n from "laravel-vue-i18n/vite";
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import i18n from 'laravel-vue-i18n/vite'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: "resources/js/app.js",
-            ssr: "resources/js/ssr.js",
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-        i18n(),
-        AutoImport({
-            dts: true,
-            dirs: ["./resources/js/Composables", "./resources/js/Utils"],
-            resolvers: [ElementPlusResolver()],
-            imports: [
-                "vue",
-                {
-                    "@inertiajs/vue3": ["useForm", "usePage", "router"],
-                },
-            ],
-        }),
-        Components({
-            dts: "auto-components.d.ts",
-            dirs: ["./resources/js/Components", "./resources/js/Layouts"],
-            resolvers: [
-                ElementPlusResolver(),
-                {
-                    resolve: (componentName) => {
-                        if (["Link", "Head"].includes(componentName)) {
-                            return {
-                                name: componentName,
-                                from: "@inertiajs/vue3",
-                            };
-                        }
-                    },
-                    type: "component",
-                },
-            ],
-        }),
-    ],
-    ssr: {
-        noExternal: ["@inertiajs/server"],
-    },
-    resolve: {
-        alias: {
-            "@": "/resources/js",
-        },
-    },
-});
+	plugins: [
+		laravel({
+			input: 'resources/js/app.js',
+			ssr: 'resources/js/ssr.js',
+			refresh: true,
+		}),
+		vue({
+			template: {
+				transformAssetUrls: {
+					base: null,
+					includeAbsolute: false,
+				},
+			},
+		}),
+		i18n(),
+		AutoImport({
+			dts: true,
+			dirs: ['./resources/js/Composables', './resources/js/Utils'],
+			resolvers: [ElementPlusResolver()],
+			vueTemplate: true,
+			imports: [
+				'vue',
+				{
+					'@inertiajs/vue3': ['useForm', 'usePage', 'router'],
+				},
+			],
+		}),
+		Components({
+			dts: 'auto-components.d.ts',
+			dirs: ['./resources/js/Components', './resources/js/Layouts'],
+			resolvers: [
+				ElementPlusResolver(),
+				{
+					resolve: (componentName) => {
+						if (['Link', 'Head'].includes(componentName)) {
+							return {
+								name: componentName,
+								from: '@inertiajs/vue3',
+							}
+						}
+					},
+					type: 'component',
+				},
+			],
+		}),
+	],
+	ssr: {
+		noExternal: ['@inertiajs/server'],
+	},
+	resolve: {
+		alias: {
+			'@': '/resources/js',
+		},
+	},
+})
