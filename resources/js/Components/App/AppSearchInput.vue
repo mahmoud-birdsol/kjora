2f17c84bdf62499cf5864985b6f467d7493782a1
amlWalaed <amlwalaed@gmail.com>
2024-10-20 14:26:35 +0300

    [feature] add input for search AppSearchInput

---
+++ resources/js/Components/App/AppSearchInput.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
+
+const { query, searchForm, search, clear } = useSearch()
+</script>
+<template>
+	<Input
+		v-model="query"
+		:placeholder="$t('search')"
+		@keydown.enter="search">
+		<template #leading>
+			<MagnifyingGlassIcon class="text-gray-400 size-4" />
+		</template>
+		<template #trailing>
+			<Button
+				aria-label="clear"
+				icon="i-radix-icons-cross-2"
+				@click="clear"
+				size="sm"
+				:class="{ invisible: !query?.length }"
+				variant="link" />
+		</template>
+	</Input>
+</template>
