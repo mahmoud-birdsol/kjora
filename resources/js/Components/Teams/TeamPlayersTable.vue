25878178354a5a928f2cc7f5d8e420cfdd0f765d
amlWalaed <amlwalaed@gmail.com>
2024-10-21 09:52:40 +0300

    [feature] use data table in TeamPlayerTable

--- resources/js/Components/Teams/TeamPlayersTable.vue
+++ resources/js/Components/Teams/TeamPlayersTable.vue
@@ -1,6 +1,31 @@
 <script setup lang="ts">
+import { createColumnHelper } from '@tanstack/vue-table'
+import { trans } from 'laravel-vue-i18n'
+import PlayerSimpleCard from '@/Components/PlayerCards/PlayerSimpleCard.vue'
+
+type Model = User
+
 const props = defineProps<{
-	players: Users
+	players: Model[]
 }>()
+
+const columnHelper = createColumnHelper<Model>()
+const columns = [
+	columnHelper.accessor('name', {
+		header: trans('player-info'),
+		cell: (context) =>
+			h(PlayerSimpleCard, {
+				player: context.row.original,
+			}),
+	}),
+	columnHelper.accessor('position.name', {
+		header: trans('position'),
+		cell: (context) => context.getValue(),
+	}),
+]
 </script>
-<template></template>
+<template>
+	<DataTable
+		:columns
+		:data="players" />
+</template>
