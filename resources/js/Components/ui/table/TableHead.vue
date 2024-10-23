81fd7489ecf39d93b6ef2d3b4412e16a66b34702
amlWalaed <amlwalaed@gmail.com>
2024-10-23 10:05:40 +0300

    [fix] remove text align left from tableHead

--- resources/js/Components/ui/table/TableHead.vue
+++ resources/js/Components/ui/table/TableHead.vue
@@ -3,12 +3,18 @@ import type { HTMLAttributes } from 'vue'
 import { cn } from '@/Utils'
 
 const props = defineProps<{
-  class?: HTMLAttributes['class']
+	class?: HTMLAttributes['class']
 }>()
 </script>
 
 <template>
-  <th :class="cn('h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0', props.class)">
-    <slot />
-  </th>
+	<th
+		:class="
+			cn(
+				'h-12 px-4 align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0',
+				props.class
+			)
+		">
+		<slot />
+	</th>
 </template>
